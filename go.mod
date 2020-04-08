803ef6e7af0d7e30be33497bacd5b8dff39ac7f9
Ling Lan <llan@redhat.com>
2020-04-08 17:38:41 -0500

    update existing configmap cluster-monitoring-config if it already exists

--- go.mod
+++ go.mod
@@ -9,8 +9,9 @@ require (
 	github.com/openshift/api v3.9.1-0.20190924102528-32369d4db2ad+incompatible
 	github.com/openshift/client-go v0.0.0-20200116152001-92a2713fa240
 	github.com/openshift/cluster-monitoring-operator v0.1.1-0.20200406225945-844f960a2a8d
-	github.com/operator-framework/operator-sdk v0.15.1
+	github.com/operator-framework/operator-sdk v0.16.0
 	github.com/spf13/pflag v1.0.5
+	gopkg.in/yaml.v2 v2.2.8
 	k8s.io/api v0.17.3
 	k8s.io/apimachinery v0.17.3
 	k8s.io/client-go v12.0.0+incompatible
--- pkg/controller/multiclustermonitoring/multiclustermonitoring_controller.go
+++ pkg/controller/multiclustermonitoring/multiclustermonitoring_controller.go
@@ -333,7 +333,7 @@ func (r *ReconcileMultiClusterMonitoring) newAPIGatewayRoute(cr *monitoringv1.Mu
 		}
 
 	} else if err == nil && len(apiGatewayServices.Items) == 0 {
-		log.Info("Cannot find the service ", cr.Name+observatoriumPartoOfName+"-"+observatoriumAPIGatewayName)
+		log.Info("Cannot find the service ", "serviceName", cr.Name+observatoriumPartoOfName+"-"+observatoriumAPIGatewayName)
 		return &reconcile.Result{RequeueAfter: time.Second * 10}, nil
 	} else {
 		return &reconcile.Result{}, err
@@ -424,14 +424,29 @@ func (r *ReconcileMultiClusterMonitoring) newOCPMonitoringCM(cr *monitoringv1.Mu
 
 	existingCM := &v1.ConfigMap{}
 	err = r.client.Get(context.TODO(), types.NamespacedName{Name: ocpMonitoringCM.Name, Namespace: ocpMonitoringCM.Namespace}, existingCM)
-	if err != nil && errors.IsNotFound(err) {
+	if err == nil {
+		log.Info("Updating the configmap for cluster monitoring")
+		err = util.UpdateConfigMap(existingCM, obsRoute.Spec.Host)
+		if err != nil {
+			log.Error(err, "Failed to update the configmap")
+			return &reconcile.Result{}, err
+		}
+		err = r.client.Update(context.TODO(), existingCM)
+		if err != nil {
+			return &reconcile.Result{}, err
+		}
+	} else if errors.IsNotFound(err) {
 		log.Info("Creating the configmap for cluster monitoring")
+		ocpMonitoringCM, err := util.CreateConfigMap(obsRoute.Spec.Host)
+		if err != nil {
+			log.Error(err, "Failed to create configmap")
+			return &reconcile.Result{}, err
+		}
 		err = r.client.Create(context.TODO(), ocpMonitoringCM)
 		if err != nil {
 			return &reconcile.Result{}, err
 		}
-		return nil, nil
-	} else if err != nil {
+	} else {
 		return &reconcile.Result{}, err
 	}
 
