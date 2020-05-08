5984aadfa9c04c0b8278f27a7f171eea98efd601
Chunlin Yang <chuyang@redhat.com>
2020-05-08 16:31:06 +0800

    Add copyright and update travis (#16)

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -6,11 +6,11 @@ echo "<repo>/<component>:<tag> : $1"
 ./tests/e2e/setup.sh $1
 if [ $? -ne 0 ]; then
     echo "Cannot setup environment successfully."
-    exit 2
+    exit 1
 fi
 
 ./tests/e2e/tests.sh
 if [ $? -ne 0 ]; then
     echo "Cannot pass all test cases."
-    exit 2
+    exit 1
 fi
