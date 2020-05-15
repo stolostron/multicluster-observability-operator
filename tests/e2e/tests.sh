bc0195ab47bf76fababd8a2961c647fc3c69d2af
clyang82 <chuyang@redhat.com>
2020-05-15 10:21:18 +0800

    refactor the tests

--- tests/e2e/tests.sh
+++ tests/e2e/tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Copyright (c) 2020 Red Hat, Inc.
 
-function wait_for_popup() {
+export WAIT_TIMEOUT=${WAIT_TIMEOUT:-5m}
+
+wait_for_popup() {
     n=1
     while true
     do
@@ -18,61 +20,85 @@ function wait_for_popup() {
     done
 }
 
-export WAIT_TIMEOUT=${WAIT_TIMEOUT:-5m}
+run_test_readiness() {
+    echo "Test to ensure all critical pods are running"
 
-echo "Test to ensure all critical pods are running"
+    MULTICLUSTER_MONITORING_CR_NAME="monitoring"
 
-MULTICLUSTER_MONITORING_CR_NAME="monitoring"
+    MULTICLUSTER_MONITORING_DEPLOYMENTS="multicluster-monitoring-operator"
+    GRAFANA_DEPLOYMENTS="grafana"
+    MINIO_DEPLOYMENTS="minio"
 
-MULTICLUSTER_MONITORING_DEPLOYMENTS="multicluster-monitoring-operator"
-GRAFANA_DEPLOYMENTS="grafana"
-MINIO_DEPLOYMENTS="minio"
+    OBSERVATORIUM_DEPLOYMENTS="$MULTICLUSTER_MONITORING_CR_NAME-observatorium-observatorium-api $MULTICLUSTER_MONITORING_CR_NAME-observatorium-observatorium-api-thanos-query $MULTICLUSTER_MONITORING_CR_NAME-observatorium-cortex-query-frontend $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-query $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-receive-controller"
 
-OBSERVATORIUM_DEPLOYMENTS="$MULTICLUSTER_MONITORING_CR_NAME-observatorium-observatorium-api $MULTICLUSTER_MONITORING_CR_NAME-observatorium-observatorium-api-thanos-query $MULTICLUSTER_MONITORING_CR_NAME-observatorium-cortex-query-frontend $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-query $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-receive-controller"
+    OBSERVATORIUM_STATEFULSET="$MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-compact $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-receive-default $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-rule $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-store-memcached $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-store-shard-0"
 
-OBSERVATORIUM_STATEFULSET="$MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-compact $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-receive-default $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-rule $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-store-memcached $MULTICLUSTER_MONITORING_CR_NAME-observatorium-thanos-store-shard-0"
+    for depl in ${MULTICLUSTER_MONITORING_DEPLOYMENTS}; do
+        if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
+            echo "$depl is not ready after $WAIT_TIMEOUT"
+            exit 1
+        fi
+    done
 
-for depl in ${MULTICLUSTER_MONITORING_DEPLOYMENTS}; do
-    if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
-        echo "$depl is not ready after $WAIT_TIMEOUT"
-        exit 1
-    fi
-done
+    echo "wait for operator is ready for reconciling..."
 
-echo "wait for operator is ready for reconciling..."
-
-for depl in ${MINIO_DEPLOYMENTS}; do
-    wait_for_popup "deployments" $depl
-    if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
-        echo "$depl is not ready after $WAIT_TIMEOUT"
-        exit 1
-    fi
-done
-
-
-for depl in ${OBSERVATORIUM_DEPLOYMENTS}; do
-    wait_for_popup "deployments" $depl
-    if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
-        echo "$depl is not ready after $WAIT_TIMEOUT"
-        exit 1
-    fi
-done
+    for depl in ${MINIO_DEPLOYMENTS}; do
+        wait_for_popup "deployments" $depl
+        if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
+            echo "$depl is not ready after $WAIT_TIMEOUT"
+            exit 1
+        fi
+    done
 
 
-for depl in ${OBSERVATORIUM_STATEFULSET}; do
-    wait_for_popup "statefulset" $depl
-    if ! kubectl -n open-cluster-management rollout status statefulset $depl --timeout=$WAIT_TIMEOUT; then 
-        echo "$depl is not ready after $WAIT_TIMEOUT"
-        exit 1
-    fi
-done
+    for depl in ${OBSERVATORIUM_DEPLOYMENTS}; do
+        wait_for_popup "deployments" $depl
+        if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
+            echo "$depl is not ready after $WAIT_TIMEOUT"
+            exit 1
+        fi
+    done
 
-for depl in ${GRAFANA_DEPLOYMENTS}; do
-    wait_for_popup "deployments" $depl
-    if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
-        echo "$depl is not ready after $WAIT_TIMEOUT"
-        exit 1
-    fi
-done
 
-./tests/e2e/grafana_tests.sh
+    for depl in ${OBSERVATORIUM_STATEFULSET}; do
+        wait_for_popup "statefulset" $depl
+        if ! kubectl -n open-cluster-management rollout status statefulset $depl --timeout=$WAIT_TIMEOUT; then 
+            echo "$depl is not ready after $WAIT_TIMEOUT"
+            exit 1
+        fi
+    done
+
+    for depl in ${GRAFANA_DEPLOYMENTS}; do
+        wait_for_popup "deployments" $depl
+        if ! kubectl -n open-cluster-management rollout status deployments $depl --timeout=$WAIT_TIMEOUT; then 
+            echo "$depl is not ready after $WAIT_TIMEOUT"
+            exit 1
+        fi
+    done
+}
+
+# test grafana replicas changes
+run_test_scale_grafana() {
+    kubectl patch MultiClusterMonitoring monitoring --patch '{"spec":{"grafana":{"replicas":2}}}' --type=merge
+
+    n=1
+    while true
+    do
+        # check there are 2 grafana pods here
+        replicas=`kubectl get deployment grafana | grep -v AVAILABLE | awk '{ print $4 }'`
+        if [[ $replicas -eq 2 ]]; then
+            echo "grafana replicas is update to 2 successfully."
+            break
+        fi
+        if [[ $n -ge 10 ]]; then
+            echo "grafana replicas changes test is failed."
+            exit 1
+        fi
+        n=$((n+1))
+        echo "Retrying in 10s..."
+        sleep 10
+    done
+}
+
+run_test_readiness
+run_test_scale_grafana
