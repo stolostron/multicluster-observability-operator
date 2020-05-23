9496b823ad6c822c45b42ccf801b7c1f2459749e
Marco <llan@redhat.com>
2020-05-22 22:54:08 -0500

    fix problems in manifestwork scenario

--- pkg/controller/util/obsAPI.go
+++ pkg/controller/util/obsAPI.go
@@ -7,7 +7,7 @@ import (
 
 	routev1 "github.com/openshift/api/route/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -15,11 +15,11 @@ const (
 )
 
 // GetObsAPIUrl is used to get the URL for observartium api gateway
-func GetObsAPIUrl(client client.Client, namespace string) (string, error) {
+func GetObsAPIUrl(client runtimeclient.Client, namespace string) (string, error) {
 	found := &routev1.Route{}
+
 	err := client.Get(context.TODO(), types.NamespacedName{Name: obsAPIGateway, Namespace: namespace}, found)
 	if err != nil {
-		log.Error(err, "Failed to get route", obsAPIGateway)
 		return "", err
 	}
 	return found.Spec.Host, nil
