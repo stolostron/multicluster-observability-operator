b40297cff987f22fed26b611ba6d77747a189c8c
clyang82 <chuyang@redhat.com>
2020-06-01 22:48:38 +0800

    add credential.helper for authenticate

--- cicd-scripts/run-unit-tests.sh
+++ cicd-scripts/run-unit-tests.sh
@@ -2,4 +2,9 @@ echo "UNIT TESTS GO HERE!"
 
 echo "<repo>/<component>:<tag> : $1"
 
+git config credential.helper store
+git config user.name ${GITHUB_USER}
+echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" >> ~/.git-credentials
+git config -l
+
 go test ./...
