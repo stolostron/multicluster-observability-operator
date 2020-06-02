b9c14a9ad5597f8d41a4e4679f34903d5cce782c
clyang82 <chuyang@redhat.com>
2020-06-02 15:09:13 +0800

    Fix git credentials issue

--- cicd-scripts/run-unit-tests.sh
+++ cicd-scripts/run-unit-tests.sh
@@ -2,4 +2,6 @@ echo "UNIT TESTS GO HERE!"
 
 echo "<repo>/<component>:<tag> : $1"
 
+git config --global url."https://$GITHUB_TOKEN@github.com/open-cluster-management".insteadOf "https://github.com/open-cluster-management"
+
 go test ./...
