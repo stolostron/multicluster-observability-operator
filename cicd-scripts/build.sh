13201d8cbae294f8fa0ec0430788ffdbbb320e06
Song Song Li <ssli@redhat.com>
2020-06-19 08:54:39 +0800

    add configfile for labels (#75)

--- cicd-scripts/build.sh
+++ cicd-scripts/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Copyright (c) 2020 Red Hat, Inc.
+set -e
 
-echo "<repo>/<component>:<tag> : $1"
+make docker-binary
 
 git config --global url."https://$GITHUB_TOKEN@github.com/open-cluster-management".insteadOf  "https://github.com/open-cluster-management"
 
-operator-sdk build $1
+echo "Building multicluster-monitoring-operator image"
+export DOCKER_IMAGE_AND_TAG=${1}
+export DOCKER_FILE=Dockerfile
+make docker/build
