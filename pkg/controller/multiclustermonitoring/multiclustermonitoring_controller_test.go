8b8f00dd37bfe2db0b3e557bd0cf4c366f590339
Chunlin Yang <chuyang@redhat.com>
2020-06-28 11:04:41 +0800

    Merge Resource (#84) (#85)
    * Merge Resource
    
    * fix grafana replicas and code smell
    
    Co-authored-by: Song Song Li <ssli@redhat.com>
    
    Co-authored-by: Song Song Li <ssli@redhat.com>

--- pkg/controller/multiclustermonitoring/multiclustermonitoring_controller_test.go
+++ pkg/controller/multiclustermonitoring/multiclustermonitoring_controller_test.go
@@ -3,10 +3,27 @@
 package multiclustermonitoring
 
 import (
+	"context"
 	"os"
+	"path"
 	"testing"
 
+	observatoriumv1alpha1 "github.com/observatorium/configuration/api/v1alpha1"
+	configv1 "github.com/openshift/api/config/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	fakeconfigclient "github.com/openshift/client-go/config/clientset/versioned/fake"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+
+	monitoringv1alpha1 "github.com/open-cluster-management/multicluster-monitoring-operator/pkg/apis/monitoring/v1alpha1"
 )
 
 func init() {
@@ -20,4 +37,140 @@ func TestLabelsForMultiClusterMonitoring(t *testing.T) {
 	if value != "test" {
 		t.Errorf("value (%v) is not the expected (test)", value)
 	}
+}
+
+func createObservatoriumApiService(name, namespace string) *corev1.Service {
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Service",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name + "-observatorium-observatorium-api",
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app.kubernetes.io/component": "api",
+				"app.kubernetes.io/instance":  name + "-observatorium",
+			},
+		},
+		Spec: corev1.ServiceSpec{},
+	}
+}
+
+func createClusterVersion() *configv1.ClusterVersion {
+	return &configv1.ClusterVersion{
+		ObjectMeta: metav1.ObjectMeta{Name: "version"},
+		Spec: configv1.ClusterVersionSpec{
+			ClusterID: configv1.ClusterID("xxx-xxxxxx-xxxx"),
+		},
+	}
+}
+
+func TestMultiClusterMonitoringCRUpdate(t *testing.T) {
+	var (
+		name               = "monitoring"
+		namespace          = "open-cluster-management-monitoring"
+		defaultStorageSize = "3Gi"
+	)
+	logf.SetLogger(logf.ZapLogger(true))
+
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Failed to get work dir: (%v)", err)
+	}
+	//provide a non-existence path to bypass the rendering
+	//cannot convert unstructured.Unstructured into v1.Service in fake client
+	os.Setenv("TEMPLATES_PATH", path.Join(wd, "../../../tests/manifests"))
+
+	// A MultiClusterMonitoring object with metadata and spec.
+	mcm := &monitoringv1alpha1.MultiClusterMonitoring{
+		TypeMeta:   metav1.TypeMeta{Kind: "MultiClusterMonitoring"},
+		ObjectMeta: metav1.ObjectMeta{Namespace: namespace, Name: name},
+		Spec:       monitoringv1alpha1.MultiClusterMonitoringSpec{},
+	}
+
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	monitoringv1alpha1.SchemeBuilder.AddToScheme(s)
+	observatoriumv1alpha1.AddToScheme(s)
+	routev1.AddToScheme(s)
+
+	svc := createObservatoriumApiService(name, namespace)
+	objs := []runtime.Object{mcm, svc}
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+
+	ocpClient := fakeconfigclient.NewSimpleClientset([]runtime.Object{createClusterVersion()}...)
+	// Create a ReconcileMemcached object with the scheme and fake client.
+	r := &ReconcileMultiClusterMonitoring{client: cl, scheme: s, ocpClient: ocpClient}
+
+	// Mock request to simulate Reconcile() being called on an event for a
+	// watched resource .
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+
+	_, err = r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+
+	updatedMCM := &monitoringv1alpha1.MultiClusterMonitoring{}
+	err = cl.Get(context.TODO(), req.NamespacedName, updatedMCM)
+	if err != nil {
+		t.Fatalf("Failed to get MultiClusterMonitoring: (%v)", err)
+	}
+	log.Info("updated MultiClusterMonitoring successfully", "MultiClusterMonitoring", updatedMCM)
+
+	// A MultiClusterMonitoring object with metadata and spec.
+	mcm = &monitoringv1alpha1.MultiClusterMonitoring{
+		TypeMeta:   metav1.TypeMeta{Kind: "MultiClusterMonitoring"},
+		ObjectMeta: metav1.ObjectMeta{Namespace: namespace, Name: name},
+		Spec: monitoringv1alpha1.MultiClusterMonitoringSpec{
+			Observatorium: &observatoriumv1alpha1.ObservatoriumSpec{
+				Compact: observatoriumv1alpha1.CompactSpec{
+					VolumeClaimTemplate: observatoriumv1alpha1.VolumeClaimTemplate{
+						Spec: corev1.PersistentVolumeClaimSpec{
+							AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceName(corev1.ResourceStorage): resource.MustParse(defaultStorageSize),
+								},
+							},
+						},
+					},
+				},
+				Rule: observatoriumv1alpha1.RuleSpec{
+					VolumeClaimTemplate: observatoriumv1alpha1.VolumeClaimTemplate{
+						Spec: corev1.PersistentVolumeClaimSpec{
+							AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceName(corev1.ResourceStorage): resource.MustParse(defaultStorageSize),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	err = cl.Update(context.TODO(), mcm)
+	if err != nil {
+		t.Fatalf("Failed to update MultiClusterMonitoring: (%v)", err)
+	}
+	_, err = r.Reconcile(req)
+	if err != nil {
+		t.Fatalf("reconcile: (%v)", err)
+	}
+	updatedMCM = &monitoringv1alpha1.MultiClusterMonitoring{}
+	err = r.client.Get(context.TODO(), req.NamespacedName, updatedMCM)
+	if err != nil {
+		t.Fatalf("Failed to get MultiClusterMonitoring: (%v)", err)
+	}
+	log.Info("updated MultiClusterMonitoring successfully", "MultiClusterMonitoring", updatedMCM)
+
 }
