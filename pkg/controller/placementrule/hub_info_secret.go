b77e393edda5157d6e541684e03157f560e4728d
Marco <llan@redhat.com>
2020-08-21 18:21:49 -0500

    check hub info secret before create it

--- pkg/controller/placementrule/hub_info_secret.go
+++ pkg/controller/placementrule/hub_info_secret.go
@@ -6,7 +6,9 @@ import (
 	"context"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
 
@@ -52,9 +54,19 @@ func createHubInfoSecret(client client.Client, obsNamespace string, namespace st
 		},
 	}
 
-	err = client.Create(context.TODO(), hubInfoSec)
-	if err != nil {
-		log.Error(err, "Failed to create hubInfo secret")
+	found := &corev1.Secret{}
+	err = client.Get(context.TODO(), types.NamespacedName{Name: hubInfoName, Namespace: namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		log.Info("Creating hubinfo secret", "namespace", namespace)
+		err = client.Create(context.TODO(), hubInfoSec)
+		if err != nil {
+			log.Error(err, "Failed to create hubInfo secret")
+		}
+		return err
+	} else if err != nil {
+		log.Error(err, "Failed to check hubinfo secret")
+		return err
 	}
-	return err
+	log.Info("hubinfo secret already existed", "namespace", namespace)
+	return nil
 }
