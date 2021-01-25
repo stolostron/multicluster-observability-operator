dc76e6f66096cc5a710e2cd8c4da97fc95eda40d
Song Song Li <ssli@redhat.com>
2021-01-25 10:37:47 +0800

    Use custom certs for kubeconfig (#299) (#324)
    * Use custom certs for kubeconfig (#299)
    
    * Use custom certs for kubeconfig
    
    * use existing GetKubeAPIServerAddress method
    
    * watch if APIServer exists
    
    * judge if apisever exists
    
    * fix code smell
    
    Co-authored-by: Chunlin Yang <chuyang@redhat.com>

--- pkg/controller/placementrule/kubeconfig.go
+++ pkg/controller/placementrule/kubeconfig.go
@@ -3,8 +3,18 @@
 package placementrule
 
 import (
+	"context"
+	"fmt"
+	"net/url"
+	"strings"
+
+	ocinfrav1 "github.com/openshift/api/config/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	clientv1 "k8s.io/client-go/tools/clientcmd/api/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
@@ -13,18 +23,37 @@ import (
 )
 
 const (
-	kubeConfigName = "hub-kube-config"
+	kubeConfigName           = "hub-kube-config"
+	infrastructureConfigName = "cluster"
+	apiserverConfigName      = "cluster"
+	openshiftConfigNamespace = "openshift-config"
 )
 
-func createKubeConfig(client client.Client, namespace string) (*clientv1.Config, error) {
-	ca, token, error := getSAToken(client, namespace)
-	if error != nil {
-		return nil, error
+func createKubeConfig(client client.Client, restMapper meta.RESTMapper, namespace string) (*clientv1.Config, error) {
+	ca, token, err := getSAToken(client, namespace)
+	if err != nil {
+		return nil, err
 	}
-	apiServer, error := config.GetKubeAPIServerAddress(client)
-	if error != nil {
-		return nil, error
+
+	apiServer, err := config.GetKubeAPIServerAddress(client)
+	if err != nil {
+		return nil, err
 	}
+	// if there is customized certs for api server, use the customized cert for kubeconfig
+	if u, err := url.Parse(apiServer); err == nil {
+		apiServerCertSecretName, err := getKubeAPIServerSecretName(client, restMapper, u.Hostname())
+		if err != nil {
+			return nil, err
+		}
+		if len(apiServerCertSecretName) > 0 {
+			apiServerCert, err := getKubeAPIServerCertificate(client, apiServerCertSecretName)
+			if err != nil {
+				return nil, err
+			}
+			ca = apiServerCert
+		}
+	}
+
 	return &clientv1.Config{
 		Kind:       "Config",
 		APIVersion: "v1",
@@ -59,8 +88,8 @@ func createKubeConfig(client client.Client, namespace string) (*clientv1.Config,
 	}, nil
 }
 
-func createKubeSecret(client client.Client, namespace string) (*corev1.Secret, error) {
-	config, err := createKubeConfig(client, namespace)
+func createKubeSecret(client client.Client, restMapper meta.RESTMapper, namespace string) (*corev1.Secret, error) {
+	config, err := createKubeConfig(client, restMapper, namespace)
 	if err != nil {
 		return nil, err
 	}
@@ -81,4 +110,83 @@ func createKubeSecret(client client.Client, namespace string) (*corev1.Secret, e
 			"kubeconfig": configYaml,
 		},
 	}, nil
+}
+
+func getKubeAPIServerAddress(client client.Client) (string, error) {
+	infraConfig := &ocinfrav1.Infrastructure{}
+
+	if err := client.Get(context.TODO(),
+		types.NamespacedName{Name: infrastructureConfigName}, infraConfig); err != nil {
+		return "", err
+	}
+
+	return infraConfig.Status.APIServerURL, nil
+}
+
+// getKubeAPIServerSecretName iterate through all namespacedCertificates
+// returns the first one which has a name matches the given dnsName
+func getKubeAPIServerSecretName(client client.Client, restMapper meta.RESTMapper, dnsName string) (string, error) {
+
+	if restMapper != nil {
+		gk := schema.GroupKind{Group: ocinfrav1.GroupVersion.Group, Kind: "APIServer"}
+		_, err := restMapper.RESTMapping(gk, ocinfrav1.GroupVersion.Version)
+		if err != nil {
+			log.Info("the server doesn't have a resource type APIServer", "error", err)
+			return "", nil
+		}
+	}
+
+	apiserver := &ocinfrav1.APIServer{}
+	if err := client.Get(
+		context.TODO(),
+		types.NamespacedName{Name: apiserverConfigName},
+		apiserver,
+	); err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("APIServer cluster not found")
+			return "", nil
+		}
+		return "", err
+	}
+	// iterate through all namedcertificates
+	for _, namedCert := range apiserver.Spec.ServingCerts.NamedCertificates {
+		for _, name := range namedCert.Names {
+			if strings.EqualFold(name, dnsName) {
+				return namedCert.ServingCertificate.Name, nil
+			}
+		}
+	}
+	return "", nil
+}
+
+// getKubeAPIServerCertificate looks for secret in openshift-config namespace, and returns tls.crt
+func getKubeAPIServerCertificate(client client.Client, secretName string) ([]byte, error) {
+	secret := &corev1.Secret{}
+	if err := client.Get(
+		context.TODO(),
+		types.NamespacedName{Name: secretName, Namespace: openshiftConfigNamespace},
+		secret,
+	); err != nil {
+		log.Error(err, fmt.Sprintf("Failed to get secret %s/%s", openshiftConfigNamespace, secretName))
+		if errors.IsNotFound(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	if secret.Type != corev1.SecretTypeTLS {
+		return nil, fmt.Errorf(
+			"secret %s/%s should have type=kubernetes.io/tls",
+			openshiftConfigNamespace,
+			secretName,
+		)
+	}
+	res, ok := secret.Data["tls.crt"]
+	if !ok {
+		return nil, fmt.Errorf(
+			"failed to find data[tls.crt] in secret %s/%s",
+			openshiftConfigNamespace,
+			secretName,
+		)
+	}
+	return res, nil
 }
