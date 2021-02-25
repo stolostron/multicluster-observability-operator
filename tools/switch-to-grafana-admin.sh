5d6c796096749f95c7212ad0d66637ab0f15babb
Song Song Li <ssli@redhat.com>
2021-02-25 23:38:04 +0800

    check python cmd

--- tools/switch-to-grafana-admin.sh
+++ tools/switch-to-grafana-admin.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 # Copyright (c) 2021 Red Hat, Inc.
 
+if command -v python &> /dev/null
+then
+    PYTHON_CMD="python"
+elif command -v python2 &> /dev/null
+then
+    PYTHON_CMD="python2"
+elif command -v python3 &> /dev/null
+then
+    PYTHON_CMD="python3"
+else
+    echo "Failed to found python command, please install firstly"
+    exit 1
+fi
+
 usage() {
   cat <<EOF
 Usage: $(basename "${BASH_SOURCE[0]}") [-h] user_name
@@ -28,13 +42,13 @@ start() {
 
   curlCMD="kubectl exec -it -n open-cluster-management-observability $podName -c grafana-dashboard-loader -- /usr/bin/curl"
   XForwardedUser="WHAT_YOU_ARE_DOING_IS_VOIDING_SUPPORT_0000000000000000000000000000000000000000000000000000000000000000"
-  userID=`$curlCMD -s -X GET -H "Content-Type: application/json" -H "X-Forwarded-User: $XForwardedUser" 127.0.0.1:3001/api/users/lookup?loginOrEmail=$1 | python -c "import sys, json; print(json.load(sys.stdin)['id'])" 2>/dev/null`
+  userID=`$curlCMD -s -X GET -H "Content-Type: application/json" -H "X-Forwarded-User: $XForwardedUser" 127.0.0.1:3001/api/users/lookup?loginOrEmail=$1 | $PYTHON_CMD -c "import sys, json; print(json.load(sys.stdin)['id'])" 2>/dev/null`
   if [ $? -ne 0 ]; then
       echo "Failed to fetch user ID, please check your user name"
       exit 1
   fi
   
-  orgID=`$curlCMD -s -X GET -H "Content-Type: application/json" -H "X-Forwarded-User:$XForwardedUser" 127.0.0.1:3001/api/users/lookup?loginOrEmail=$1 | python -c "import sys, json; print(json.load(sys.stdin)['orgId'])" 2>/dev/null`
+  orgID=`$curlCMD -s -X GET -H "Content-Type: application/json" -H "X-Forwarded-User:$XForwardedUser" 127.0.0.1:3001/api/users/lookup?loginOrEmail=$1 | $PYTHON_CMD -c "import sys, json; print(json.load(sys.stdin)['orgId'])" 2>/dev/null`
   if [ $? -ne 0 ]; then
       echo "Failed to fetch organization ID, please check your user name"
       exit 1
