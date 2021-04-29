2505292339591b6649cddbd5b1fe638353668726
clyang82 <chuyang@redhat.com>
2021-04-29 09:49:04 +0800

    if replicas is less than 3, then ReplicationFactor equals to replicas. if replicas is greater than 3, the ReplicationFactor equals to 3
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- controllers/multiclusterobservability/observatorium.go
+++ controllers/multiclusterobservability/observatorium.go
@@ -321,7 +321,12 @@ func newReceiversSpec(
 	receSpec := obsv1alpha1.ReceiversSpec{}
 	receSpec.Retention = mco.Spec.RetentionConfig.RetentionInLocal
 	receSpec.Replicas = mcoconfig.GetObservabilityComponentReplicas(mcoconfig.ThanosReceive)
-	receSpec.ReplicationFactor = &config.Replicas3
+	if *receSpec.Replicas < 3 {
+		receSpec.ReplicationFactor = receSpec.Replicas
+	} else {
+		receSpec.ReplicationFactor = &config.Replicas3
+	}
+
 	receSpec.ServiceMonitor = true
 	if !mcoconfig.WithoutResourcesRequests(mco.GetAnnotations()) {
 		receSpec.Resources = mco.Spec.Resources.ThanosReceive
