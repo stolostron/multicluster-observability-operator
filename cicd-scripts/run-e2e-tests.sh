9e3db94e9e16323ff89e321a2d3c4576fb00d515
Marco <llan@redhat.com>
2021-05-03 14:27:43 -0500

    debug

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -2,7 +2,6 @@
 # Copyright (c) 2021 Red Hat, Inc.
 # Copyright Contributors to the Open Cluster Management project
 
-git clone --depth 1 https://github.com/marcolan018/observability-e2e-test.git
+git clone --depth 1 https://github.com/open-cluster-management/observability-e2e-test.git
 cd observability-e2e-test
-git checkout debug
 make test-e2e
