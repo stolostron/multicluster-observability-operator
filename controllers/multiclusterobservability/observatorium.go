5fe2c3354641987c4f8ec763e074227f820de058
Marco <llan@redhat.com>
2021-05-03 02:49:46 -0500

    debug: revert changes for golang upgrade

--- controllers/multiclusterobservability/observatorium.go
+++ controllers/multiclusterobservability/observatorium.go
@@ -485,6 +485,8 @@ func newThanosSpec(mco *mcov1beta2.MultiClusterObservability, scSelected string)
 	if replace {
 		thanosSpec.Image = image
 	}
+	thanosSpec.Version = "2.3.0-SNAPSHOT-2021-04-27-14-40-48"
+	thanosSpec.Image = "quay.io/open-cluster-management/thanos:2.3.0-SNAPSHOT-2021-04-27-14-40-48"
 	return thanosSpec
 }
 
