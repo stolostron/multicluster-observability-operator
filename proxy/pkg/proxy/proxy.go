f4fcec0271c78ad0de4905de9e8bd5b4f2c4c435
clyang82 <chuyang@redhat.com>
2021-06-24 10:00:39 +0800

    move proxy from open-cluster-management/rbac-query-proxy for testing
    Signed-off-by: clyang82 <chuyang@redhat.com>

---
+++ proxy/pkg/proxy/proxy.go
@@ -0,0 +1,155 @@
+// Copyright (c) 2021 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package proxy
+
+import (
+	"bytes"
+	"compress/gzip"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+
+	"k8s.io/klog"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	"github.com/open-cluster-management/rbac-query-proxy/pkg/util"
+)
+
+const (
+	basePath        = "/api/metrics/v1/default"
+	projectsAPIPath = "/apis/project.openshift.io/v1/projects"
+	userAPIPath     = "/apis/user.openshift.io/v1/users/~"
+)
+
+var (
+	serverScheme = ""
+	serverHost   = ""
+)
+
+// HandleRequestAndRedirect is used to init proxy handler
+func HandleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
+	if preCheckRequest(req) != nil {
+		_, err := res.Write(newEmptyMatrixHTTPBody())
+		if err != nil {
+			klog.Errorf("failed to write response: %v", err)
+		}
+		return
+	}
+
+	serverURL, err := url.Parse(os.Getenv("METRICS_SERVER"))
+	if err != nil {
+		klog.Errorf("failed to parse url: %v", err)
+	}
+	serverHost = serverURL.Host
+	serverScheme = serverURL.Scheme
+
+	tlsTransport, err := getTLSTransport()
+	if err != nil {
+		klog.Fatalf("failed to create tls transport: %v", err)
+	}
+
+	// create the reverse proxy
+	proxy := httputil.ReverseProxy{
+		Director:  proxyRequest,
+		Transport: tlsTransport,
+	}
+
+	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+	req.Host = serverURL.Host
+	req.URL.Path = path.Join(basePath, req.URL.Path)
+	util.ModifyMetricsQueryParams(req, config.GetConfigOrDie().Host+projectsAPIPath)
+	proxy.ServeHTTP(res, req)
+}
+
+func errorHandle(rw http.ResponseWriter, req *http.Request, err error) {
+	token := req.Header.Get("X-Forwarded-Access-Token")
+	if token == "" {
+		rw.WriteHeader(http.StatusUnauthorized)
+	}
+}
+
+func preCheckRequest(req *http.Request) error {
+	token := req.Header.Get("X-Forwarded-Access-Token")
+	if token == "" {
+		token = req.Header.Get("Authorization")
+		if token == "" {
+			return errors.New("found unauthorized user")
+		} else {
+			req.Header.Set("X-Forwarded-Access-Token", token)
+		}
+	}
+
+	userName := req.Header.Get("X-Forwarded-User")
+	if userName == "" {
+		userName = util.GetUserName(token, config.GetConfigOrDie().Host+userAPIPath)
+		if userName == "" {
+			return errors.New("failed to found user name")
+		} else {
+			req.Header.Set("X-Forwarded-User", userName)
+		}
+	}
+
+	projectList, ok := util.GetUserProjectList(token)
+	if !ok {
+		projectList = util.FetchUserProjectList(token, config.GetConfigOrDie().Host+projectsAPIPath)
+		up := util.NewUserProject(userName, token, projectList)
+		util.UpdateUserProject(up)
+	}
+
+	if len(projectList) == 0 || len(util.GetAllManagedClusterNames()) == 0 {
+		return errors.New("no project or cluster found")
+	}
+
+	return nil
+}
+
+func newEmptyMatrixHTTPBody() []byte {
+	var bodyBuff bytes.Buffer
+	gz := gzip.NewWriter(&bodyBuff)
+	if _, err := gz.Write([]byte(`{"status":"success","data":{"resultType":"matrix","result":[]}}`)); err != nil {
+		klog.Errorf("failed to write body: %v", err)
+	}
+
+	if err := gz.Close(); err != nil {
+		klog.Errorf("failed to close gzip writer: %v", err)
+	}
+
+	var gzipBuff bytes.Buffer
+	err := gzipWrite(&gzipBuff, bodyBuff.Bytes())
+	if err != nil {
+		klog.Errorf("failed to write with gizp: %v", err)
+	}
+
+	return gzipBuff.Bytes()
+}
+
+func gzipWrite(w io.Writer, data []byte) error {
+	gw, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+	if err != nil {
+		return err
+	}
+	defer gw.Close()
+	_, err = gw.Write(data)
+	return err
+}
+
+func proxyRequest(r *http.Request) {
+	r.URL.Scheme = serverScheme
+	r.URL.Host = serverHost
+	if r.Method == http.MethodGet {
+		if strings.HasSuffix(r.URL.Path, "/api/v1/query") ||
+			strings.HasSuffix(r.URL.Path, "/api/v1/query_range") ||
+			strings.HasSuffix(r.URL.Path, "/api/v1/series") {
+			r.Method = http.MethodPost
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			r.Body = ioutil.NopCloser(strings.NewReader(r.URL.RawQuery))
+		}
+	}
+}
---
+++ proxy/pkg/proxy/proxy_test.go
@@ -0,0 +1,185 @@
+// Copyright (c) 2021 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package proxy
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/open-cluster-management/rbac-query-proxy/pkg/util"
+)
+
+func TestNewEmptyMatrixHTTPBody(t *testing.T) {
+	body := newEmptyMatrixHTTPBody()
+	gr, err := gzip.NewReader(bytes.NewBuffer([]byte(body)))
+	defer gr.Close()
+	data, err := ioutil.ReadAll(gr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var decompressedBuff bytes.Buffer
+	gr, err = gzip.NewReader(bytes.NewBuffer([]byte(data)))
+	defer gr.Close()
+	data, err = ioutil.ReadAll(gr)
+	if err != nil {
+		t.Errorf("failed to ReadAll: %v", err)
+	}
+
+	decompressedBuff.Write(data)
+	emptyMatrix := `{"status":"success","data":{"resultType":"matrix","result":[]}}`
+	if decompressedBuff.String() != emptyMatrix {
+		t.Errorf("(%v) is not the expected: (%v)", decompressedBuff.String(), emptyMatrix)
+	}
+}
+
+type FakeResponse struct {
+	t       *testing.T
+	headers http.Header
+	body    []byte
+	status  int
+}
+
+func NewFakeResponse(t *testing.T) *FakeResponse {
+	return &FakeResponse{
+		t:       t,
+		headers: make(http.Header),
+	}
+}
+
+func (r *FakeResponse) Header() http.Header {
+	return r.headers
+}
+
+func (r *FakeResponse) Write(body []byte) (int, error) {
+	r.body = body
+	return len(body), nil
+}
+
+func (r *FakeResponse) WriteHeader(status int) {
+	r.status = status
+}
+
+func TestErrorHandle(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://127.0.0.1:3002/metrics/query?query=foo", nil)
+	req.Header.Set("X-Forwarded-User", "test")
+	var err error
+	fakeResp := NewFakeResponse(t)
+	errorHandle(fakeResp, req, err)
+	if fakeResp.status != http.StatusUnauthorized {
+		t.Errorf("failed to get expected status: %v", fakeResp.status)
+	}
+}
+
+func TestPreCheckRequest(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://127.0.0.1:3002/metrics/query?query=foo", nil)
+	resp := http.Response{
+		Body:    ioutil.NopCloser(bytes.NewBufferString("test")),
+		Header:  make(http.Header),
+		Request: req,
+	}
+	resp.Request.Header.Set("X-Forwarded-Access-Token", "test")
+	resp.Request.Header.Set("X-Forwarded-User", "test")
+	util.InitUserProjectInfo()
+	up := util.NewUserProject("test", "test", []string{"p"})
+	util.UpdateUserProject(up)
+	util.InitAllManagedClusterNames()
+	clusters := util.GetAllManagedClusterNames()
+	clusters["p"] = "p"
+	err := preCheckRequest(req)
+	if err != nil {
+		t.Errorf("failed to test preCheckRequest: %v", err)
+	}
+
+	resp.Request.Header.Del("X-Forwarded-Access-Token")
+	resp.Request.Header.Add("Authorization", "test")
+	err = preCheckRequest(req)
+	if err != nil {
+		t.Errorf("failed to test preCheckRequest with bear token: %v", err)
+	}
+
+	resp.Request.Header.Del("X-Forwarded-User")
+	err = preCheckRequest(req)
+	if !strings.Contains(err.Error(), "failed to found user name") {
+		t.Errorf("failed to test preCheckRequest: %v", err)
+	}
+
+	resp.Request.Header.Del("X-Forwarded-Access-Token")
+	resp.Request.Header.Del("Authorization")
+	err = preCheckRequest(req)
+	if !strings.Contains(err.Error(), "found unauthorized user") {
+		t.Errorf("failed to test preCheckRequest: %v", err)
+	}
+
+}
+
+func TestGzipWrite(t *testing.T) {
+	originalStr := "test"
+	var compressedBuff bytes.Buffer
+	err := gzipWrite(&compressedBuff, []byte(originalStr))
+	if err != nil {
+		t.Errorf("failed to compressed: %v", err)
+	}
+	var decompressedBuff bytes.Buffer
+	gr, err := gzip.NewReader(bytes.NewBuffer(compressedBuff.Bytes()))
+	defer gr.Close()
+	data, err := ioutil.ReadAll(gr)
+	if err != nil {
+		t.Errorf("failed to decompressed: %v", err)
+	}
+	decompressedBuff.Write(data)
+	if decompressedBuff.String() != originalStr {
+		t.Errorf("(%v) is not the expected: (%v)", originalStr, decompressedBuff.String())
+	}
+}
+
+func TestProxyRequest(t *testing.T) {
+	req := http.Request{}
+	req.URL = &url.URL{}
+	req.Header = http.Header(map[string][]string{})
+	proxyRequest(&req)
+	if req.Body != nil {
+		t.Errorf("(%v) is not the expected nil", req.Body)
+	}
+	if req.Header.Get("Content-Type") != "" {
+		t.Errorf("(%v) is not the expected: (\"\")", req.Header.Get("Content-Type"))
+	}
+
+	req.Method = http.MethodGet
+	pathList := []string{
+		"/api/v1/query",
+		"/api/v1/query_range",
+		"/api/v1/series",
+	}
+
+	for _, path := range pathList {
+		req.URL.Path = path
+		proxyRequest(&req)
+		if req.Method != http.MethodPost {
+			t.Errorf("(%v) is not the expected: (%v)", http.MethodPost, req.Method)
+		}
+
+		if req.Header.Get("Content-Type") != "application/x-www-form-urlencoded" {
+			t.Errorf("(%v) is not the expected: (%v)", req.Header.Get("Content-Type"), "application/x-www-form-urlencoded")
+		}
+
+		if req.Body == nil {
+			t.Errorf("(%v) is not the expected non-nil", req.Body)
+		}
+
+		if req.URL.Scheme != "" {
+			t.Errorf("(%v) is not the expected \"\"", req.URL.Scheme)
+		}
+
+		if req.URL.Host != "" {
+			t.Errorf("(%v) is not the expected \"\"", req.URL.Host)
+		}
+	}
+}
