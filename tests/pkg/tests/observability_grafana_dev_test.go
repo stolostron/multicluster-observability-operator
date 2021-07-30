09d341950807dfca08d33b72e46c933e50dfd6a1
Song Song Li <ssli@redhat.com>
2021-07-30 11:11:43 +0800

    enable test in 2.4
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- tests/pkg/tests/observability_grafana_dev_test.go
+++ tests/pkg/tests/observability_grafana_dev_test.go
@@ -19,7 +19,6 @@ var _ = Describe("Observability:", func() {
 	// Do not need to run this case in canary environment
 	// If we really need it in canary, ensure the grafana-dev-test.sh is available in observability-e2e-test image and all required commands exist
 	It("[P1][Sev1][Observability][Integration] Should run grafana-dev test successfully (grafana-dev/g0)", func() {
-		Skip("Skip grafana-dev temporarily. need more tests")
 		cmd := exec.Command("../../cicd-scripts/grafana-dev-test.sh")
 		var out bytes.Buffer
 		cmd.Stdout = &out
