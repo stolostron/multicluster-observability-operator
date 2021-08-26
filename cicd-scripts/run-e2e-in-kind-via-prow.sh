f88530381d406b31afefda110022b7928df6b228
Song Song Li <ssli@redhat.com>
2021-08-27 00:19:09 +0800

    check env
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- cicd-scripts/run-e2e-in-kind-via-prow.sh
+++ cicd-scripts/run-e2e-in-kind-via-prow.sh
@@ -15,6 +15,7 @@ OPT=(-q -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$KEY
 ssh "${OPT[@]}" "$HOST" kind create cluster --kubeconfig /tmp/kind-kubeconfig --name kind > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" kind version > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" kubectl cluster-info --kubeconfig /tmp/kind-kubeconfig > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
+ssh "${OPT[@]}" "$HOST" env
 
 # scp "${OPT[@]}" tests/run-in-kind/run-e2e-in-kind.sh "$HOST:/tmp/run-e2e-in-kind.sh"
 # ssh "${OPT[@]}" "$HOST" /tmp/run-e2e-in-kind.sh > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
