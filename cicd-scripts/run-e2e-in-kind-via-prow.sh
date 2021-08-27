5487a6065adebc065faa7b692d6f7f825ecf2c1e
Song Song Li <ssli@redhat.com>
2021-08-27 09:53:44 +0800

    debug env from prow
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- cicd-scripts/run-e2e-in-kind-via-prow.sh
+++ cicd-scripts/run-e2e-in-kind-via-prow.sh
@@ -15,6 +15,8 @@ OPT=(-q -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$KEY
 ssh "${OPT[@]}" "$HOST" kind create cluster --kubeconfig /tmp/kind-kubeconfig --name kind > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" kind version > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" kubectl cluster-info --kubeconfig /tmp/kind-kubeconfig > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
+ssh "${OPT[@]}" "$HOST" export MULTICLUSTER_OBSERVABILITY_OPERATOR_IMAGE_REF="$MULTICLUSTER_OBSERVABILITY_OPERATOR_IMAGE_REF" > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
+ssh "${OPT[@]}" "$HOST" export ENDPOINT_MONITORING_OPERATOR_IMAGE_REF="$ENDPOINT_MONITORING_OPERATOR_IMAGE_REF" > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" env
 
 # scp "${OPT[@]}" tests/run-in-kind/run-e2e-in-kind.sh "$HOST:/tmp/run-e2e-in-kind.sh"
