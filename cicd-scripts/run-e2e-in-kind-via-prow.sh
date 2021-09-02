68b1fa5c24167d1ff9384413b491a93c74bbed56
Song Song Li <ssli@redhat.com>
2021-09-02 11:00:46 +0800

    auto commit
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- cicd-scripts/run-e2e-in-kind-via-prow.sh
+++ cicd-scripts/run-e2e-in-kind-via-prow.sh
@@ -15,9 +15,10 @@ OPT=(-q -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$KEY
 ssh "${OPT[@]}" "$HOST" kind create cluster --kubeconfig /tmp/kind-kubeconfig --name kind > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" kind version > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" kubectl cluster-info --kubeconfig /tmp/kind-kubeconfig > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
-ssh "${OPT[@]}" "$HOST" export MULTICLUSTER_OBSERVABILITY_OPERATOR_IMAGE_REF="$MULTICLUSTER_OBSERVABILITY_OPERATOR_IMAGE_REF" > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
-ssh "${OPT[@]}" "$HOST" export ENDPOINT_MONITORING_OPERATOR_IMAGE_REF="$ENDPOINT_MONITORING_OPERATOR_IMAGE_REF" > >(tee "$ARTIFACT_DIR/run-e2e-in-kind.log") 2>&1
 ssh "${OPT[@]}" "$HOST" env
+ssh "${OPT[@]}" "$HOST" "go get -u github.com/onsi/ginkgo/ginkgo"
+ssh "${OPT[@]}" "$HOST" "$(go env GOPATH)/bin/ginkgo version"
+
 ssh "${OPT[@]}" "$HOST" sleep 100000
 
 # scp "${OPT[@]}" tests/run-in-kind/run-e2e-in-kind.sh "$HOST:/tmp/run-e2e-in-kind.sh"
