b454be0390aafe330dbadca52b55c100f01d99d0
Song Song Li <ssli@redhat.com>
2021-09-23 16:56:30 +0800

    add ut for custom registry
    Signed-off-by: Song Song Li <ssli@redhat.com>

--- operators/multiclusterobservability/controllers/placementrule/manifestwork_test.go
+++ operators/multiclusterobservability/controllers/placementrule/manifestwork_test.go
@@ -5,22 +5,27 @@ package placementrule
 
 import (
 	"context"
+	"encoding/base64"
 	"os"
 	"path"
 	"reflect"
+	"strings"
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
+	"github.com/open-cluster-management/multicloud-operators-foundation/pkg/apis/imageregistry/v1alpha1"
 	mcoshared "github.com/open-cluster-management/multicluster-observability-operator/operators/multiclusterobservability/api/shared"
 	mcov1beta2 "github.com/open-cluster-management/multicluster-observability-operator/operators/multiclusterobservability/api/v1beta2"
 	"github.com/open-cluster-management/multicluster-observability-operator/operators/multiclusterobservability/pkg/config"
 	operatorconfig "github.com/open-cluster-management/multicluster-observability-operator/operators/pkg/config"
+	clusterv1 "open-cluster-management.io/api/cluster/v1"
 	workv1 "open-cluster-management.io/api/work/v1"
 )
 
@@ -145,6 +150,132 @@ func NewAmAccessorTokenSecret() *corev1.Secret {
 			"token": []byte("xxxxx"),
 		},
 	}
+}
+
+func newCluster(name string, labels map[string]string) *clusterv1.ManagedCluster {
+	return &clusterv1.ManagedCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   name,
+			Labels: labels,
+		},
+	}
+}
+
+func newPullSecret(name, namespace string, data []byte) *corev1.Secret {
+	return &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: map[string][]byte{
+			corev1.DockerConfigJsonKey: data,
+		},
+		StringData: nil,
+		Type:       corev1.SecretTypeDockerConfigJson,
+	}
+}
+
+func newImageRegistry(name, namespace, registryAddress, pullSecret string) *v1alpha1.ManagedClusterImageRegistry {
+	return &v1alpha1.ManagedClusterImageRegistry{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: v1alpha1.ImageRegistrySpec{
+			Registry:     registryAddress,
+			PullSecret:   corev1.LocalObjectReference{Name: pullSecret},
+			PlacementRef: v1alpha1.PlacementRef{},
+		},
+	}
+}
+
+func TestManifestWorkForCustomRegistry(t *testing.T) {
+	initSchema(t)
+	objs := []runtime.Object{
+		newTestObsApiRoute(),
+		newTestAlertmanagerRoute(),
+		newTestIngressController(),
+		newTestRouteCASecret(),
+		newCASecret(),
+		newCertSecret(mcoNamespace),
+		NewMetricsAllowListCM(),
+		NewMetricsCustomAllowListCM(),
+		NewAmAccessorSA(),
+		NewAmAccessorTokenSecret(),
+		newCluster(clusterName, map[string]string{v1alpha1.ClusterImageRegistryLabel: namespace + ".image_registry"}),
+		newImageRegistry("image_registry", namespace, "registry_server", "custorm_pull_secret"),
+		newPullSecret("custorm_pull_secret", namespace, []byte("custorm")),
+	}
+	c := fake.NewFakeClient(objs...)
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Failed to get work dir: (%v)", err)
+	}
+	os.MkdirAll(path.Join(wd, "../../tests"), 0755)
+	testManifestsPath := path.Join(wd, "../../tests/manifests")
+	manifestsPath := path.Join(wd, "../../manifests")
+	os.Setenv("TEMPLATES_PATH", testManifestsPath)
+	err = os.Symlink(manifestsPath, testManifestsPath)
+	if err != nil {
+		t.Fatalf("Failed to create symbollink(%s) to(%s) for the test manifests: (%v)", testManifestsPath, manifestsPath, err)
+	}
+
+	// set the default pull secret
+	pullSecret = newPullSecret("multiclusterhub-operator-pull-secret", namespace, []byte("default"))
+	// config the managedcluster to use the custom registry
+	managedClusterImageRegistry[clusterName] = "open-cluster-management.io/image-registry=" + namespace + ".image_registry"
+
+	works, crdWork, _, err := generateGlobalManifestResources(c, newTestMCO())
+	if err != nil {
+		t.Fatalf("Failed to get global manifestwork resourc: (%v)", err)
+	}
+
+	if hubInfoSecret, err = generateHubInfoSecret(c, config.GetDefaultNamespace(), spokeNameSpace, true); err != nil {
+		t.Fatalf("Failed to generate hubInfo secret: (%v)", err)
+	}
+
+	err = createManifestWorks(c, nil, namespace, clusterName, newTestMCO(), works, crdWork, endpointMetricsOperatorDeploy, hubInfoSecret, false)
+	if err != nil {
+		t.Fatalf("Failed to create manifestworks: (%v)", err)
+	}
+	found := &workv1.ManifestWork{}
+	workName := namespace + workNameSuffix
+	err = c.Get(context.TODO(), types.NamespacedName{Name: workName, Namespace: namespace}, found)
+	if err != nil {
+		t.Fatalf("Failed to get manifestwork %s: (%v)", workName, err)
+	}
+
+	// To check pullsecret, endpoint-observability-operator and image list configmap
+	for _, manifest := range found.Spec.Workload.Manifests {
+		obj := &unstructured.Unstructured{}
+		obj.UnmarshalJSON(manifest.Raw)
+		if obj.GetKind() == "Secret" && obj.GetName() == "multiclusterhub-operator-pull-secret" {
+			if !strings.Contains(string(manifest.Raw), base64.StdEncoding.EncodeToString([]byte("custorm"))) {
+				t.Errorf("multiclusterhub-operator-pull-secret should use the custom pull secret")
+			}
+		}
+
+		if obj.GetKind() == "ConfigMap" && obj.GetName() == "images-list" {
+			if !strings.Contains(string(manifest.Raw), "registry_server") {
+				t.Errorf("images-list should use the custom registry image")
+			}
+		}
+
+		if obj.GetKind() == "Deployment" && obj.GetName() == "endpoint-observability-operator" {
+			if !strings.Contains(string(manifest.Raw), "registry_server") {
+				t.Errorf("endpoint-observability-operator should use the custom registry image")
+			}
+		}
+	}
+
+	if err = os.Remove(testManifestsPath); err != nil {
+		t.Fatalf("Failed to delete symbollink(%s) for the test manifests: (%v)", testManifestsPath, err)
+	}
+	os.Remove(path.Join(wd, "../../tests"))
 }
 
 func TestManifestWork(t *testing.T) {
