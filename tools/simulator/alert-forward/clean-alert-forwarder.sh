4c7f3af0357bb1c5cd193a7c39959ea52af123bc
Morven Cao <lcao@redhat.com>
2021-11-11 13:43:41 +0800

    enhance the alert-forwarder for simulator. (#815)
    * enhance the alert-forwarder for simulator.
    
    Signed-off-by: morvencao <lcao@redhat.com>
    
    * add more knobs for alert forward simulator.
    
    Signed-off-by: morvencao <lcao@redhat.com>
    
    * update README.
    
    Signed-off-by: morvencao <lcao@redhat.com>
    
    * update readme.
    
    Signed-off-by: morvencao <lcao@redhat.com>

---
+++ tools/simulator/alert-forward/clean-alert-forwarder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (c) 2021 Red Hat, Inc.
+# Copyright Contributors to the Open Cluster Management project
+
+KUBECTL="kubectl"
+if ! command -v kubectl &> /dev/null; then
+    if command -v oc &> /dev/null; then
+        KUBECTL="oc"
+    else
+        echo "kubectl or oc must be installed!"
+        exit 1
+    fi
+fi
+
+ALERT_FORWARDER_NS="alert-forwarder"
+ALERT_FORWARDER_DEPLOY="alert-forwarder"
+AM_ACCESS_TOKEN_SECRET="am-access-token"
+
+${KUBECTL} -n ${ALERT_FORWARDER_NS} delete deployment ${ALERT_FORWARDER_DEPLOY}
+${KUBECTL} -n ${ALERT_FORWARDER_NS} delete secret ${AM_ACCESS_TOKEN_SECRET}
+${KUBECTL} delete ns ${ALERT_FORWARDER_NS}
