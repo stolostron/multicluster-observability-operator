50534d95bb4ae4f4790d5e8b591b108350466256
quchangl-github <cqu@redhat.com>
2021-11-24 08:56:32 +0800

    rebase and resolve conflict
    Signed-off-by: quchangl-github <cqu@redhat.com>

---
+++ tests/pkg/observability_alert_test.go
@@ -0,0 +1,252 @@
+// Copyright (c) 2021 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package tests
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+
+	"github.com/open-cluster-management/multicluster-observability-operator/tests/pkg/kustomize"
+	"github.com/open-cluster-management/multicluster-observability-operator/tests/pkg/utils"
+)
+
+var _ = Describe("Observability:", func() {
+	BeforeEach(func() {
+		hubClient = utils.NewKubeClient(
+			testOptions.HubCluster.ClusterServerURL,
+			testOptions.KubeConfig,
+			testOptions.HubCluster.KubeContext)
+
+		dynClient = utils.NewKubeClientDynamic(
+			testOptions.HubCluster.ClusterServerURL,
+			testOptions.KubeConfig,
+			testOptions.HubCluster.KubeContext)
+	})
+	statefulsetLabels := [...]string{
+		ALERTMANAGER_LABEL,
+		THANOS_RULE_LABEL,
+	}
+	configmap := [...]string{
+		"thanos-ruler-default-rules",
+		"thanos-ruler-custom-rules",
+	}
+	secret := "alertmanager-config"
+
+	It("@BVT - [P1][Sev1][Observability][Stable] Verify alert is created and received - Should have the expected statefulsets (alert/g0)", func() {
+		By("Checking if STS: Alertmanager and observability-thanos-rule exist")
+		for _, label := range statefulsetLabels {
+			sts, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).List(context.TODO(), metav1.ListOptions{LabelSelector: label})
+			Expect(err).NotTo(HaveOccurred())
+			for _, stsInfo := range (*sts).Items {
+				Expect(len(stsInfo.Spec.Template.Spec.Volumes)).Should(BeNumerically(">", 0))
+
+				if strings.Contains(stsInfo.Name, "-alertmanager") {
+					By("The statefulset: " + stsInfo.Name + " should have the appropriate secret mounted")
+					Expect(stsInfo.Spec.Template.Spec.Volumes[0].Secret.SecretName).To(Equal("alertmanager-config"))
+				}
+
+				if strings.Contains(stsInfo.Name, "-thanos-rule") {
+					By("The statefulset: " + stsInfo.Name + " should have the appropriate configmap mounted")
+					Expect(stsInfo.Spec.Template.Spec.Volumes[0].ConfigMap.Name).To(Equal("thanos-ruler-default-rules"))
+				}
+			}
+		}
+	})
+
+	It("[P2][Sev2][Observability][Stable] Verify alert is created and received - Should have the expected configmap (alert/g0)", func() {
+		By("Checking if CM: thanos-ruler-default-rules is existed")
+		cm, err := hubClient.CoreV1().ConfigMaps(MCO_NAMESPACE).Get(context.TODO(), configmap[0], metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(cm.ResourceVersion).ShouldNot(BeEmpty())
+		klog.V(3).Infof("Configmap %s does exist", configmap[0])
+	})
+
+	It("[P3][Sev3][Observability][Stable] Verify alert is created and received - Should not have the CM: thanos-ruler-custom-rules (alert/g0)", func() {
+		By("Checking if CM: thanos-ruler-custom-rules not existed")
+		_, err := hubClient.CoreV1().ConfigMaps(MCO_NAMESPACE).Get(context.TODO(), configmap[1], metav1.GetOptions{})
+
+		if err == nil {
+			err = fmt.Errorf("%s exist within the namespace env", configmap[1])
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		Expect(err).To(HaveOccurred())
+		klog.V(3).Infof("Configmap %s does not exist", configmap[1])
+	})
+
+	It("@BVT - [P1][Sev1][Observability][Stable] Verify alert is created and received - Should have the expected secret (alert/g0)", func() {
+		By("Checking if SECRETS: alertmanager-config is existed")
+		secret, err := hubClient.CoreV1().Secrets(MCO_NAMESPACE).Get(context.TODO(), secret, metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(secret.GetName()).To(Equal("alertmanager-config"))
+		klog.V(3).Infof("Successfully got secret: %s", secret.GetName())
+	})
+
+	It("@BVT - [P1][Sev1][Observability][Stable] Verify alert is created and received - Should have the alertmanager configured in rule (alert/g0)", func() {
+		By("Checking if --alertmanagers.url or --alertmanager.config or --alertmanagers.config-file is configured in rule")
+		rules, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: THANOS_RULE_LABEL,
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(rules.Items)).NotTo(Equal(0))
+		argList := (*rules).Items[0].Spec.Template.Spec.Containers[0].Args
+		exists := false
+		for _, arg := range argList {
+			if arg == "--alertmanagers.url=http://alertmanager:9093" {
+				exists = true
+				break
+			}
+			if strings.HasPrefix(arg, "--alertmanagers.config=") {
+				exists = true
+				break
+			}
+			if strings.HasPrefix(arg, "--alertmanagers.config-file=") {
+				exists = true
+				break
+			}
+		}
+		Expect(exists).To(Equal(true))
+		klog.V(3).Info("Have the alertmanager url configured in rule")
+	})
+
+	It("[P2][Sev2][Observability][Stable] Verify alert is created and received - Should have custom alert generated (alert/g0)", func() {
+		By("Creating custom alert rules")
+
+		rules, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: THANOS_RULE_LABEL,
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(rules.Items)).NotTo(Equal(0))
+
+		stsName := (*rules).Items[0].Name
+		oldSts, _ := utils.GetStatefulSet(testOptions, true, stsName, MCO_NAMESPACE)
+
+		yamlB, err := kustomize.Render(kustomize.Options{KustomizationPath: "../../../examples/alerts/custom_rules_valid"})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(utils.Apply(testOptions.HubCluster.ClusterServerURL, testOptions.KubeConfig, testOptions.HubCluster.KubeContext, yamlB)).NotTo(HaveOccurred())
+
+		ThanosRuleRestarting := false
+		By("Wait for thanos rule pods are restarted and ready")
+		// ensure the thanos rule pods are restarted successfully before processing
+		Eventually(func() error {
+			if !ThanosRuleRestarting {
+				newSts, _ := utils.GetStatefulSet(testOptions, true, stsName, MCO_NAMESPACE)
+				if oldSts.GetResourceVersion() == newSts.GetResourceVersion() {
+					return fmt.Errorf("The %s is not being restarted in 10 minutes", stsName)
+				} else {
+					ThanosRuleRestarting = true
+				}
+			}
+
+			err = utils.CheckStatefulSetPodReady(testOptions, stsName)
+			if err != nil {
+				return err
+			}
+			return nil
+		}, EventuallyTimeoutMinute*10, EventuallyIntervalSecond*5).Should(Succeed())
+
+		var labelName, labelValue string
+		labels, err := kustomize.GetLabels(yamlB)
+		Expect(err).NotTo(HaveOccurred())
+		for labelName = range labels.(map[string]interface{}) {
+			labelValue = labels.(map[string]interface{})[labelName].(string)
+		}
+
+		By("Checking alert generated")
+		Eventually(func() error {
+			err, _ := utils.ContainManagedClusterMetric(testOptions, `ALERTS{`+labelName+`="`+labelValue+`"}`,
+				[]string{`"__name__":"ALERTS"`, `"` + labelName + `":"` + labelValue + `"`})
+			return err
+		}, EventuallyTimeoutMinute*5, EventuallyIntervalSecond*5).Should(Succeed())
+	})
+
+	It("[P2][Sev2][Observability][Stable] Should modify the SECRET: alertmanager-config (alert/g0)", func() {
+		By("Editing the secret, we should be able to add the third partying tools integrations")
+		secret := utils.CreateCustomAlertConfigYaml(testOptions.HubCluster.BaseDomain)
+
+		Expect(utils.Apply(testOptions.HubCluster.ClusterServerURL, testOptions.KubeConfig, testOptions.HubCluster.KubeContext, secret)).NotTo(HaveOccurred())
+		klog.V(3).Infof("Successfully modified the secret: alertmanager-config")
+	})
+
+	It("[P2][Sev2][Observability][Stable] Updated alert rule can take effect automatically - Should have custom alert updated (alert/g0)", func() {
+		By("Updating custom alert rules")
+
+		yamlB, _ := kustomize.Render(kustomize.Options{KustomizationPath: "../../../examples/alerts/custom_rules_invalid"})
+		Expect(utils.Apply(testOptions.HubCluster.ClusterServerURL, testOptions.KubeConfig, testOptions.HubCluster.KubeContext, yamlB)).NotTo(HaveOccurred())
+
+		var labelName, labelValue string
+		labels, _ := kustomize.GetLabels(yamlB)
+		for labelName = range labels.(map[string]interface{}) {
+			labelValue = labels.(map[string]interface{})[labelName].(string)
+		}
+
+		By("Checking alert generated")
+		Eventually(func() error {
+			err, _ := utils.ContainManagedClusterMetric(testOptions, `ALERTS{`+labelName+`="`+labelValue+`"}`,
+				[]string{`"__name__":"ALERTS"`, `"` + labelName + `":"` + labelValue + `"`})
+			return err
+		}, EventuallyTimeoutMinute*5, EventuallyIntervalSecond*5).Should(MatchError("Failed to find metric name from response"))
+	})
+
+	It("[P2][Sev2][Observability][Stable] Updated alert rule can take effect automatically - delete the customized rules (alert/g0)", func() {
+
+		rules, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).List(context.TODO(), metav1.ListOptions{
+			LabelSelector: THANOS_RULE_LABEL,
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(rules.Items)).NotTo(Equal(0))
+
+		stsName := (*rules).Items[0].Name
+
+		oldSts, _ := utils.GetStatefulSet(testOptions, true, stsName, MCO_NAMESPACE)
+		Eventually(func() error {
+			err := hubClient.CoreV1().ConfigMaps(MCO_NAMESPACE).Delete(context.TODO(), configmap[1], metav1.DeleteOptions{})
+			return err
+		}, EventuallyTimeoutMinute*1, EventuallyIntervalSecond*1).Should(Succeed())
+
+		ThanosRuleRestarting := false
+		By("Wait for thanos rule pods are restarted and ready")
+		// ensure the thanos rule pods are restarted successfully before processing
+		Eventually(func() error {
+			if !ThanosRuleRestarting {
+				newSts, _ := utils.GetStatefulSet(testOptions, true, stsName, MCO_NAMESPACE)
+				if oldSts.GetResourceVersion() == newSts.GetResourceVersion() {
+					return fmt.Errorf("The %s is not being restarted in 10 minutes", stsName)
+				} else {
+					ThanosRuleRestarting = true
+				}
+			}
+
+			err = utils.CheckStatefulSetPodReady(testOptions, stsName)
+			if err != nil {
+				return err
+			}
+			return nil
+		}, EventuallyTimeoutMinute*10, EventuallyIntervalSecond*5).Should(Succeed())
+
+		klog.V(3).Infof("Successfully deleted CM: thanos-ruler-custom-rules")
+	})
+
+	JustAfterEach(func() {
+		Expect(utils.IntegrityChecking(testOptions)).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		if CurrentGinkgoTestDescription().Failed {
+			utils.PrintMCOObject(testOptions)
+			utils.PrintAllMCOPodsStatus(testOptions)
+			utils.PrintAllOBAPodsStatus(testOptions)
+		}
+		testFailed = testFailed || CurrentGinkgoTestDescription().Failed
+	})
+})
---
+++ tests/pkg/observability_config_test.go
@@ -0,0 +1,236 @@
+// Copyright (c) 2021 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package tests
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+
+	"github.com/open-cluster-management/multicluster-observability-operator/tests/pkg/utils"
+)
+
+var _ = Describe("Observability:", func() {
+	BeforeEach(func() {
+		hubClient = utils.NewKubeClient(
+			testOptions.HubCluster.ClusterServerURL,
+			testOptions.KubeConfig,
+			testOptions.HubCluster.KubeContext)
+
+		dynClient = utils.NewKubeClientDynamic(
+			testOptions.HubCluster.ClusterServerURL,
+			testOptions.KubeConfig,
+			testOptions.HubCluster.KubeContext)
+	})
+
+	It("@BVT - [P1][Sev1][Observability][Stable] Verify metrics data global setting on the managed cluster (config/g0)", func() {
+		if os.Getenv("SKIP_INSTALL_STEP") == "true" {
+			Skip("Skip the case due to MCO CR was created customized")
+		}
+		mcoRes, err := dynClient.Resource(utils.NewMCOGVRV1BETA2()).Get(context.TODO(), MCO_CR_NAME, metav1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		observabilityAddonSpec := mcoRes.Object["spec"].(map[string]interface{})["observabilityAddonSpec"].(map[string]interface{})
+		Expect(observabilityAddonSpec["enableMetrics"]).To(Equal(true))
+		Expect(observabilityAddonSpec["interval"]).To(Equal(int64(30)))
+	})
+
+	It("@BVT - [P1][Sev1][Observability][Stable] Verify MCO CR storage class and PVC (config/g0)", func() {
+		if os.Getenv("SKIP_INSTALL_STEP") == "true" {
+			Skip("Skip the case due to MCO CR was created customized")
+		}
+		mcoSC, err := dynClient.Resource(utils.NewMCOGVRV1BETA2()).Get(context.TODO(), MCO_CR_NAME, metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		spec := mcoSC.Object["spec"].(map[string]interface{})
+		scInCR := spec["storageConfig"].(map[string]interface{})["storageClass"].(string)
+
+		scList, _ := hubClient.StorageV1().StorageClasses().List(context.TODO(), metav1.ListOptions{})
+		scMatch := false
+		defaultSC := ""
+		for _, sc := range scList.Items {
+			if sc.Annotations["storageclass.kubernetes.io/is-default-class"] == "true" {
+				defaultSC = sc.Name
+			}
+			if sc.Name == scInCR {
+				scMatch = true
+			}
+		}
+		expectedSC := defaultSC
+		if scMatch {
+			expectedSC = scInCR
+		}
+
+		Eventually(func() error {
+			pvcList, err := hubClient.CoreV1().PersistentVolumeClaims(MCO_NAMESPACE).List(context.TODO(), metav1.ListOptions{})
+			if err != nil {
+				return err
+			}
+			for _, pvc := range pvcList.Items {
+				//for KinD cluster, we use minio as object storage. the size is 1Gi.
+				if pvc.GetName() != "minio" {
+					scName := *pvc.Spec.StorageClassName
+					statusPhase := pvc.Status.Phase
+					if scName != expectedSC || statusPhase != "Bound" {
+						return fmt.Errorf("PVC check failed, scName = %s, expectedSC = %s, statusPhase = %s", scName, expectedSC, statusPhase)
+					}
+				}
+			}
+			return nil
+		}, EventuallyTimeoutMinute*3, EventuallyIntervalSecond*5).Should(Succeed())
+	})
+
+	componentMap := map[string]struct {
+		// deployment or statefulset
+		Type  string
+		Label string
+	}{
+		"alertmanager": {
+			Type:  "Statefulset",
+			Label: ALERTMANAGER_LABEL,
+		},
+		"grafana": {
+			Type:  "Deployment",
+			Label: GRAFANA_LABEL,
+		},
+		"observatoriumAPI": {
+			Type:  "Deployment",
+			Label: OBSERVATORIUM_API_LABEL,
+		},
+		"rbacQueryProxy": {
+			Type:  "Deployment",
+			Label: RBAC_QUERY_PROXY_LABEL,
+		},
+		"compact": {
+			Type:  "Statefulset",
+			Label: THANOS_COMPACT_LABEL,
+		},
+		"query": {
+			Type:  "Deployment",
+			Label: THANOS_QUERY_LABEL,
+		},
+		"queryFrontend": {
+			Type:  "Deployment",
+			Label: THANOS_QUERY_FRONTEND_LABEL,
+		},
+		"queryFrontendMemcached": {
+			Type:  "Statefulset",
+			Label: THANOS_QUERY_FRONTEND_MEMCACHED_LABEL,
+		},
+		"receive": {
+			Type:  "Statefulset",
+			Label: THANOS_RECEIVE_LABEL,
+		},
+		"rule": {
+			Type:  "Statefulset",
+			Label: THANOS_RULE_LABEL,
+		},
+		"storeMemcached": {
+			Type:  "Statefulset",
+			Label: THANOS_STORE_MEMCACHED_LABEL,
+		},
+		"store": {
+			Type:  "Statefulset",
+			Label: THANOS_STORE_LABEL,
+		},
+	}
+
+	It("@BVT - [P1][Sev1][Observability][Integration] Verify the replica in advanced config for Observability components (config/g0)", func() {
+
+		mcoRes, err := dynClient.Resource(utils.NewMCOGVRV1BETA2()).Get(context.TODO(), MCO_CR_NAME, metav1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		spec := mcoRes.Object["spec"].(map[string]interface{})
+		if _, adv := spec["advanced"]; !adv {
+			Skip("Skip the case since the MCO CR did not have advanced spec configed")
+		}
+
+		advancedSpec := mcoRes.Object["spec"].(map[string]interface{})["advanced"].(map[string]interface{})
+
+		for key, component := range componentMap {
+			if key == "compact" || key == "store" {
+				continue
+			}
+			klog.V(1).Infof("The component is: %s\n", key)
+			replicas := advancedSpec[key].(map[string]interface{})["replicas"]
+			if component.Type == "Deployment" {
+				deploys, err := utils.GetDeploymentWithLabel(testOptions, true, component.Label, MCO_NAMESPACE)
+				Expect(err).NotTo(HaveOccurred())
+				for _, deployInfo := range (*deploys).Items {
+					Expect(int(replicas.(int64))).To(Equal(int(*deployInfo.Spec.Replicas)))
+				}
+			} else {
+				sts, err := utils.GetStatefulSetWithLabel(testOptions, true, component.Label, MCO_NAMESPACE)
+				Expect(err).NotTo(HaveOccurred())
+				for _, stsInfo := range (*sts).Items {
+					Expect(int(replicas.(int64))).To(Equal(int(*stsInfo.Spec.Replicas)))
+				}
+			}
+		}
+	})
+
+	It("[P2][Sev2][Observability][Integration] Persist advance values in MCO CR - Checking resources in advanced config (config/g0)", func() {
+		mcoRes, err := dynClient.Resource(utils.NewMCOGVRV1BETA2()).Get(context.TODO(), MCO_CR_NAME, metav1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		spec := mcoRes.Object["spec"].(map[string]interface{})
+		if _, adv := spec["advanced"]; !adv {
+			Skip("Skip the case since the MCO CR did not have advanced spec configed")
+		}
+
+		advancedSpec := mcoRes.Object["spec"].(map[string]interface{})["advanced"].(map[string]interface{})
+
+		for key, component := range componentMap {
+			klog.V(1).Infof("The component is: %s\n", key)
+			resources := advancedSpec[key].(map[string]interface{})["resources"]
+			limits := resources.(map[string]interface{})["limits"].(map[string]interface{})
+			var cpu string
+			switch v := limits["cpu"].(type) {
+			case int64:
+				cpu = fmt.Sprint(v)
+			default:
+				cpu = limits["cpu"].(string)
+			}
+			if component.Type == "Deployment" {
+				deploys, err := utils.GetDeploymentWithLabel(testOptions, true, component.Label, MCO_NAMESPACE)
+				Expect(err).NotTo(HaveOccurred())
+				for _, deployInfo := range (*deploys).Items {
+					Expect(cpu).To(Equal(deployInfo.Spec.Template.Spec.Containers[0].Resources.Limits.Cpu().String()))
+					Expect(limits["memory"]).To(Equal(deployInfo.Spec.Template.Spec.Containers[0].Resources.Limits.Memory().String()))
+				}
+			} else {
+				sts, err := utils.GetStatefulSetWithLabel(testOptions, true, component.Label, MCO_NAMESPACE)
+				Expect(err).NotTo(HaveOccurred())
+				for _, stsInfo := range (*sts).Items {
+					Expect(cpu).To(Equal(stsInfo.Spec.Template.Spec.Containers[0].Resources.Limits.Cpu().String()))
+					Expect(limits["memory"]).To(Equal(stsInfo.Spec.Template.Spec.Containers[0].Resources.Limits.Memory().String()))
+				}
+			}
+		}
+	})
+
+	JustAfterEach(func() {
+		Expect(utils.IntegrityChecking(testOptions)).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		if CurrentGinkgoTestDescription().Failed {
+			utils.PrintMCOObject(testOptions)
+			utils.PrintAllMCOPodsStatus(testOptions)
+			utils.PrintAllOBAPodsStatus(testOptions)
+		}
+		testFailed = testFailed || CurrentGinkgoTestDescription().Failed
+	})
+})
