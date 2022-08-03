3cd1fa6860a4feee50fda2bdf243291fe9929869
Siddharth Rajaraman <71802560+SiddharthRajaraman@users.noreply.github.com>
2022-08-03 15:18:09 -0400

    decode error checking (#1080)
    Signed-off-by: siddharthrajaraman <srajaram@redhat.com>

---
+++ operators/pkg/util/util_test.go
@@ -0,0 +1,167 @@
+package util
+
+import (
+	"encoding/base64"
+	"os"
+	"testing"
+)
+
+//Siddharth's code
+func TestRemove(t *testing.T) {
+	type testCaseList struct {
+		name     string
+		list     []string
+		s        string
+		expected []string
+	}
+
+	var testCaseLists = []testCaseList{
+		{"Should return string with 'test' removed", []string{"test", "test1", "test2"}, "test", []string{"test1", "test2"}},
+		{"Should return identical list", []string{"test", "test1", "test2"}, "test3", []string{"test", "test1", "test2"}},
+		{"Null String", []string{"", "test", "test1"}, "", []string{"test", "test1"}},
+	}
+
+	for _, test := range testCaseLists {
+		output := Remove(test.list, test.s)
+
+		for i, str := range output {
+			if str != test.expected[i] {
+				t.Errorf("case (%v) output: (%v) is not the expected: (%v)", test.name, output, test.expected)
+			}
+		}
+	}
+}
+
+func TestContains(t *testing.T) {
+	testCaseList := []struct {
+		name     string
+		list     []string
+		s        string
+		expected bool
+	}{
+		{"contain sub string", []string{"a", "b"}, "a", true},
+		{"shoud contain empty string", []string{""}, "", true},
+		{"should not contain sub string", []string{"a", "b"}, "c", false},
+		{"shoud not contain empty string", []string{"a", "b"}, "", false},
+	}
+
+	for _, c := range testCaseList {
+		output := Contains(c.list, c.s)
+		if output != c.expected {
+			t.Errorf("case (%v) output: (%v) is not the expected: (%v)", c.name, output, c.expected)
+		}
+	}
+}
+
+//Siddharth's code
+func TestGetAnnotation(t *testing.T) {
+	type testCaseList struct {
+		name       string
+		annotation map[string]string
+		key        string
+		expected   string
+	}
+	//tests whether func returns correct value for a given key
+	fullMap := map[string]string{
+		"a": "aTest",
+		"b": "bTest",
+		"c": "cTest",
+	}
+
+	//tests whether func returns nil for a nil map
+	var nilMap map[string]string
+
+	var testCaseLists = []testCaseList{
+		{"Fundamental key-value pair", fullMap, "b", "bTest"},
+		{"Nil Map", nilMap, "", ""},
+	}
+
+	for _, test := range testCaseLists {
+		output := GetAnnotation(test.annotation, test.key)
+
+		if output != test.expected {
+			t.Errorf("case (%v) output: (%v) is not the expected: (%v)", test.name, output, test.expected)
+		}
+	}
+}
+
+//func TestGeneratePassword
+func TestGeneratePassword(t *testing.T) {
+	type testCaseList struct {
+		name     string
+		n        int
+		expected error
+	}
+	var testCaseLists = []testCaseList{
+		{"=0", 0, nil},
+		{">0", 4, nil},
+	}
+
+	for _, test := range testCaseLists {
+		output, err := GeneratePassword(test.n)
+		_, err1 := base64.StdEncoding.DecodeString(output)
+
+		if err != nil {
+			t.Errorf("case (%v) output: (%v) is not the expected: (%v)", test.name, output, test.expected)
+		}
+		if err1 != nil {
+			t.Errorf("case (%v) output: (%v) is not the expected: (%v)", test.name, output, test.expected)
+		}
+
+	}
+}
+
+func setEnvVars(http, https, no_proxy string) {
+	os.Setenv("HTTP_PROXY", http)
+	os.Setenv("HTTPS_PROXY", https)
+	os.Setenv("NO_PROXY", no_proxy)
+
+}
+func TestProxyEnvVarsAreSet(t *testing.T) {
+	type testCaseList struct {
+		name     string
+		http     string
+		https    string
+		no_proxy string
+		expected bool
+	}
+	var testCaseLists = []testCaseList{
+		{"set http", "test", "", "", true},
+		{"set https", "", "test", "", true},
+		{"set no_proxy", "", "", "test", true},
+		{"all null str", "", "", "", false},
+	}
+
+	for _, test := range testCaseLists {
+		setEnvVars(test.http, test.https, test.no_proxy)
+		output := ProxyEnvVarsAreSet()
+
+		if output != test.expected {
+			t.Errorf("case {%v), output: (%v) is not expected: (%v)", test.name, output, test.expected)
+		}
+	}
+}
+
+//Siddharth's code
+func TestRemoveDuplicates(t *testing.T) {
+	type testCaseList struct {
+		name     string
+		elements []string
+		expected []string
+	}
+	var testCaseLists = []testCaseList{
+		{"One duplicate pair", []string{"a", "b", "a", "c", "d"}, []string{"a", "b", "c", "d"}},
+		{"Two duplicate pairs", []string{"a", "b", "c", "d", "c", "a"}, []string{"a", "b", "c", "d"}},
+		{"No duplicates", []string{"a", "b", "c", "d"}, []string{"a", "b", "c", "d"}},
+	}
+
+	for _, test := range testCaseLists {
+		output := RemoveDuplicates(test.elements)
+
+		for i, str := range output {
+			if str != test.expected[i] {
+				t.Errorf("case {%v), output: (%v) is not expected: (%v)", test.name, output, test.expected)
+			}
+		}
+	}
+}
