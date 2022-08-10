75e01a3b8b8562f51d6184f7018785ea4ed4210c
Marco <llan@redhat.com>
2022-08-10 15:31:23 -0500

    use alertmanager pod address list in thanos config, to replace alertmgr svc address Signed-off-by: marcolan018 <llan@redhat.com>

--- operators/multiclusterobservability/pkg/rendering/renderer_thanos.go
+++ operators/multiclusterobservability/pkg/rendering/renderer_thanos.go
@@ -4,16 +4,24 @@
 package rendering
 
 import (
+	"strconv"
+
+	"github.com/thanos-io/thanos/pkg/alert"
+	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/kustomize/v3/pkg/resource"
 
+	mcoconfig "github.com/stolostron/multicluster-observability-operator/operators/multiclusterobservability/pkg/config"
 	rendererutil "github.com/stolostron/multicluster-observability-operator/operators/pkg/rendering"
+	"github.com/stolostron/multicluster-observability-operator/operators/pkg/util"
 )
 
 func (r *MCORenderer) newThanosRenderer() {
 	r.renderThanosFns = map[string]rendererutil.RenderFn{
 		"ServiceAccount":     r.renderer.RenderNamespace,
-		"ConfigMap":          r.renderer.RenderNamespace,
+		"ConfigMap":          r.RenderThanosConfig,
 		"ClusterRole":        r.renderer.RenderClusterRole,
 		"ClusterRoleBinding": r.renderer.RenderClusterRoleBinding,
 		"Secret":             r.renderer.RenderNamespace,
@@ -41,4 +49,46 @@ func (r *MCORenderer) renderThanosTemplates(templates []*resource.Resource,
 	}
 
 	return uobjs, nil
+}
+
+func (r *MCORenderer) RenderThanosConfig(res *resource.Resource,
+	namespace string, labels map[string]string) (*unstructured.Unstructured, error) {
+	u, err := r.renderer.RenderNamespace(res, namespace, labels)
+	if err != nil {
+		return nil, err
+	}
+	if u.GetName() == "thanos-ruler-config" {
+		obj := util.GetK8sObj(u.GetKind())
+		err = runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj)
+		if err != nil {
+			return nil, err
+		}
+		cm := obj.(*corev1.ConfigMap)
+		alertingConfig := &alert.AlertingConfig{}
+		err = yaml.Unmarshal([]byte(cm.Data["config.yaml"]), alertingConfig)
+		if err != nil {
+			log.Error(err, "Failed to unmarshal data in configmap thanos-ruler-config")
+			return nil, err
+		}
+		addr := []string{}
+		for i := 0; i < int(*mcoconfig.GetReplicas("alertmanager", r.cr.Spec.AdvancedConfig)); i++ {
+			addr = append(addr, "observability-alertmanager-"+strconv.Itoa(i)+".alertmanager-operated.open-cluster-management-observability.svc:9095")
+		}
+		alertingConfig.Alertmanagers[0].EndpointsConfig.StaticAddresses = addr
+		updateConfig, err := yaml.Marshal(alertingConfig)
+		if err != nil {
+			log.Error(err, "Failed to marshal data")
+			return nil, err
+		}
+		cm.Data["config.yaml"] = string(updateConfig)
+
+		unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+		if err != nil {
+			return nil, err
+		}
+
+		return &unstructured.Unstructured{Object: unstructuredObj}, nil
+	}
+
+	return u, nil
 }
