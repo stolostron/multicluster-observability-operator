e9703a17856979a0850f24ad40a0ea1c8246c5b6
dislbenn <dbennett@redhat.com>
2022-10-25 16:37:33 -0400

    updated proxy main
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -4,16 +4,29 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
 	"os"
 
+	corev1 "k8s.io/api/core/v1"
+	k8serror "k8s.io/apimachinery/pkg/api/errors"
+
 	"github.com/spf13/pflag"
+
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog"
+
+	"gopkg.in/yaml.v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 
+	mcoConfig "github.com/stolostron/multicluster-observability-operator/operators/multiclusterobservability/pkg/config"
+	proxyCfg "github.com/stolostron/multicluster-observability-operator/proxy/pkg/config"
 	"github.com/stolostron/multicluster-observability-operator/proxy/pkg/proxy"
 	"github.com/stolostron/multicluster-observability-operator/proxy/pkg/util"
+
 	clusterclientset "open-cluster-management.io/api/client/cluster/clientset/versioned"
 )
 
@@ -28,7 +41,6 @@ type proxyConf struct {
 }
 
 func main() {
-
 	cfg := proxyConf{}
 
 	klogFlags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
@@ -58,6 +70,39 @@ func main() {
 	if err != nil {
 		klog.Fatalf("failed to new cluster clientset: %v", err)
 	}
+
+	// Build the config for the client
+	config, err := clientcmd.BuildConfigFromFlags("", "")
+
+	c, err := client.New(config, client.Options{})
+	if err != nil {
+		klog.Fatalf("failed to initialize client: %v", err)
+	}
+
+	configmap := proxyCfg.CreateClusterLabelConfigmap()
+
+	found := &corev1.ConfigMap{}
+	err = c.Get(context.TODO(), types.NamespacedName{
+		Namespace: mcoConfig.GetDefaultNamespace(),
+		Name:      proxyCfg.GetManagedClusterLabelConfigMapName()}, found)
+
+	if err != nil {
+		if k8serror.IsNotFound(err) {
+			err = c.Create(context.TODO(), configmap)
+
+			if err != nil {
+				klog.Errorf("Failed to get %s configmap", proxyCfg.GetManagedClusterLabelConfigMapName())
+			}
+		}
+	}
+
+	labels := found.Data[proxyCfg.GetManagedClusterLabelConfigMapKey()]
+	klog.Infof("Labels: %v", labels)
+
+	labelList := proxyCfg.GetClusterLabelList()
+	err = yaml.Unmarshal([]byte(found.Data[proxyCfg.GetManagedClusterLabelConfigMapKey()]), labelList)
+
+	klog.Infof("ClusterLabelList: %v", labelList)
 
 	// watch all managed clusters
 	go util.WatchManagedCluster(clusterClient)
