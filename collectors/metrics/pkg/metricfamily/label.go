fc7becdc8608c2c889363afd8d38ed22258c4310
Nathaniel Graham <ngraham@redhat.com>
2022-11-18 14:23:49 -0700

    Blank labels removed and apended ones done Lexicographically
    Signed-off-by: Nathaniel Graham <ngraham@redhat.com>

--- collectors/metrics/pkg/metricfamily/label.go
+++ collectors/metrics/pkg/metricfamily/label.go
@@ -53,17 +53,40 @@ func appendLabels(
 	existing []*clientmodel.LabelPair,
 	overrides map[string]*clientmodel.LabelPair) []*clientmodel.LabelPair {
 	var found []string
+
+	// override matching existing labels
 	for i, pair := range existing {
 		name := pair.GetName()
+		// remove any name == "" or value == nil
+		if name == "" || pair != nil {
+			existing = append(existing[:i], existing[i+1:]...)
+		}
 		if value, ok := overrides[name]; ok {
 			existing[i] = value
 			found = append(found, name)
 		}
 	}
+
+	// append any overrides that didn't already exist
 	for k, v := range overrides {
-		if !contains(found, k) {
-			existing = append(existing, v)
+		// only append real names and values
+		// don't append anything overwritten above
+		if k != "" && v != nil && !contains(found, k) {
+			existing = insertLexicographicallyByName(existing, v)
 		}
+	}
+	return existing
+}
+
+func insertLexicographicallyByName(
+	existing []*clientmodel.LabelPair,
+	value *clientmodel.LabelPair) []*clientmodel.LabelPair {
+
+	existing = append(existing, value)
+	i := len(existing) - 1
+	for existing[i].GetName() < existing[i-1].GetName() {
+		existing[i], existing[i-1] = existing[i-1], existing[i]
+		i -= 1
 	}
 	return existing
 }
