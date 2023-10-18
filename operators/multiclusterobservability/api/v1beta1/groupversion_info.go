ffe19cf68be90bfcd8fe96bed5995080d189c5ab
Saswata Mukherjee <saswataminsta@yahoo.com>
2023-10-18 23:25:05 +0530

    Make file copyright consistent and automated (#1261)
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- operators/multiclusterobservability/api/v1beta1/groupversion_info.go
+++ operators/multiclusterobservability/api/v1beta1/groupversion_info.go
@@ -1,20 +1,6 @@
-// Copyright (c) 2021 Red Hat, Inc.
+// Copyright (c) Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
-/*
-Copyright 2021.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Licensed under the Apache License 2.0
 
 // Package v1beta1 contains API Schema definitions for the observability v1beta1 API group
 // +kubebuilder:object:generate=true
