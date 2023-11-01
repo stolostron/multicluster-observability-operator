9edaa1f4f11eb6cf5de982b767eee71ec3ab3ba5
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2023-11-01 04:16:44 +0100

    Clear `gosec` false positives (#1275)
    * Clear up gosec false positives
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>
    
    * Correclty skip gosec false positive
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- operators/multiclusterobservability/pkg/rendering/renderer_proxy.go
+++ operators/multiclusterobservability/pkg/rendering/renderer_proxy.go
@@ -125,7 +125,7 @@ func (r *MCORenderer) renderProxySecret(res *resource.Resource,
 		return nil, err
 	}
 
-	if u.GetName() == "rbac-proxy-cookie-secret" {
+	if u.GetName() == "rbac-proxy-cookie-secret" { // #nosec G101  -- false positive, not a hardcoded credential
 		obj := util.GetK8sObj(u.GetKind())
 		err = runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj)
 		if err != nil {
