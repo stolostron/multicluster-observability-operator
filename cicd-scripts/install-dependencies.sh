e2e59d8a43c2f454e87c16f72f482a605f62822f
Saswata Mukherjee <saswataminsta@yahoo.com>
2023-12-11 14:18:45 +0530

    Upgrade operator-sdk, controller-gen and kustomize with latest configs (#1291)
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- cicd-scripts/install-dependencies.sh
+++ cicd-scripts/install-dependencies.sh
@@ -4,13 +4,15 @@
 
 echo "install dependencies"
 
-_OPERATOR_SDK_VERSION=v1.4.2
+_OPERATOR_SDK_VERSION=v1.32.0
 
 if ! [ -x "$(command -v operator-sdk)" ]; then
   if [[ $OSTYPE == "linux-gnu" ]]; then
     curl -L https://github.com/operator-framework/operator-sdk/releases/download/${_OPERATOR_SDK_VERSION}/operator-sdk_linux_amd64 -o operator-sdk
-  elif [[ $OSTYPE == "darwin"* ]]; then
+  elif [[ $OSTYPE == "darwin-amd64"* ]]; then
     curl -L https://github.com/operator-framework/operator-sdk/releases/download/${_OPERATOR_SDK_VERSION}/operator-sdk_darwin_amd64 -o operator-sdk
+  elif [[ $OSTYPE == "darwin-arm64"* ]]; then
+    curl -L https://github.com/operator-framework/operator-sdk/releases/download/${_OPERATOR_SDK_VERSION}/operator-sdk_darwin_arm64 -o operator-sdk
   fi
   chmod +x operator-sdk
   sudo mv operator-sdk /usr/local/bin/operator-sdk
