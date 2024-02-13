0d8ec018c0aa453829c51596a1854996c8d6ecb6
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-02-13 11:54:58 +0100

    test
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- operators/multiclusterobservability/controllers/placementrule/namespace.go
+++ operators/multiclusterobservability/controllers/placementrule/namespace.go
@@ -5,14 +5,9 @@
 package placementrule
 
 import (
-	"context"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"os"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"os"
 
 	operatorconfig "github.com/stolostron/multicluster-observability-operator/operators/pkg/config"
 )
@@ -34,31 +29,4 @@ func generateNamespace() *corev1.Namespace {
 			},
 		},
 	}
-}
-
-func generateLocalClusterNamespace(r client.Client) error {
-	namespace := &corev1.Namespace{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: corev1.SchemeGroupVersion.String(),
-			Kind:       "Namespace",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: localClusterName,
-			Annotations: map[string]string{
-				operatorconfig.WorkloadPartitioningNSAnnotationsKey: operatorconfig.WorkloadPartitioningNSExpectedValue,
-			},
-		},
-	}
-	found := &corev1.Namespace{}
-	err := r.Get(context.TODO(), types.NamespacedName{Name: localClusterName}, found)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			err = r.Create(context.TODO(), namespace)
-			if err != nil {
-				log.Error(err, "Failed to create namespace", "namespace", namespace.Name)
-				return err
-			}
-		}
-	}
-	return nil
 }
