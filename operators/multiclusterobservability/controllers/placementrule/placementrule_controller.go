1b44c5b5bad0b9e48fe0c8cc69b61d1ffa5c328c
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-02-21 00:26:53 +0100

    Update placementrule_controller.go

--- operators/multiclusterobservability/controllers/placementrule/placementrule_controller.go
+++ operators/multiclusterobservability/controllers/placementrule/placementrule_controller.go
@@ -178,6 +178,7 @@ func (r *PlacementRuleReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	if err != nil {
 		reqLogger.Error(err, "Failed to list observabilityaddon resource")
 		return ctrl.Result{}, err
+	}	
     
 	if installMetricsWithoutAddon {
 		obsAddonList.Items = append(obsAddonList.Items, mcov1beta1.ObservabilityAddon{
