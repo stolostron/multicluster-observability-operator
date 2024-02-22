a828756404f0f7eb4c4a3848db8e35a463fea118
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-02-23 00:50:53 +0100

    resource refactor
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- operators/multiclusterobservability/controllers/placementrule/placementrule_controller.go
+++ operators/multiclusterobservability/controllers/placementrule/placementrule_controller.go
@@ -191,7 +191,7 @@ func (r *PlacementRuleReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 				},
 			},
 		})
-		config.HubMetricsCollectorResources = *config.GetOBAResources(mco.Spec.ObservabilityAddonSpec)
+		operatorconfig.HubMetricsCollectorResources = *config.GetOBAResources(mco.Spec.ObservabilityAddonSpec)
 	}
 
 	if !deleteAll {
