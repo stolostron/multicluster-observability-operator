083fbfcff65f5ac5f6d6a181e4940480832d9d69
Philip Gough <philip.p.gough@gmail.com>
2024-02-27 14:21:33 +0000

    make: lint
    Signed-off-by: Philip Gough <philip.p.gough@gmail.com>

--- cicd-scripts/install-dependencies.sh
+++ cicd-scripts/install-dependencies.sh
@@ -7,7 +7,7 @@ echo "install dependencies"
 _OPERATOR_SDK_VERSION=v1.4.2
 
 if ! [ -x "$(command -v operator-sdk)" ]; then
-  curl -L "https://github.com/operator-framework/operator-sdk/releases/download/${_OPERATOR_SDK_VERSION}/operator-sdk_$(uname | tr '[:upper:]' '[:lower:]' )_$(uname -p)64" -o operator-sdk
+  curl -L "https://github.com/operator-framework/operator-sdk/releases/download/${_OPERATOR_SDK_VERSION}/operator-sdk_$(uname | tr '[:upper:]' '[:lower:]')_$(uname -p)64" -o operator-sdk
   chmod +x operator-sdk
   sudo mv operator-sdk /usr/local/bin/operator-sdk
 fi
