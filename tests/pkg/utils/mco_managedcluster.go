eba6ad394ec8d4833acbf73b04d922324ec50b30
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-02-27 16:21:57 +0100

    e2e-kind
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- tests/pkg/utils/mco_managedcluster.go
+++ tests/pkg/utils/mco_managedcluster.go
@@ -7,7 +7,6 @@ package utils
 import (
 	"context"
 	"errors"
-	"k8s.io/klog"
 
 	goversion "github.com/hashicorp/go-version"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -53,11 +52,6 @@ func ListManagedClusters(opt TestOptions) ([]string, error) {
 		metadata := obj.Object["metadata"].(map[string]interface{})
 		name := metadata["name"].(string)
 		labels := metadata["labels"].(map[string]interface{})
-		if name == "local-cluster" {
-			klog.V(1).Infof("Skip OBA status for managedcluster: %v", cluster)
-			continue
-			// no obs addon on local-cluster
-		}
 		if labels != nil {
 			obsControllerStr := ""
 			if obsController, ok := labels["feature.open-cluster-management.io/addon-observability-controller"]; ok {
