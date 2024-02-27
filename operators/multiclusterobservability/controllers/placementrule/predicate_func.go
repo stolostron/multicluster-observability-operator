6b2ef554d2c842582a4c11e54c00d906a0d2fabf
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-02-27 23:18:02 +0100

    avoid addon namespace
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- operators/multiclusterobservability/controllers/placementrule/predicate_func.go
+++ operators/multiclusterobservability/controllers/placementrule/predicate_func.go
@@ -24,9 +24,8 @@ func getClusterPreds() predicate.Funcs {
 	createFunc := func(e event.CreateEvent) bool {
 		log.Info("CreateFunc", "managedCluster", e.Object.GetName())
 		if e.Object.GetName() == "local-cluster" {
-			delete(managedClusterList, "local-cluster")
+			return false
 		}
-
 		if isAutomaticAddonInstallationDisabled(e.Object) {
 			return false
 		}
@@ -40,7 +39,9 @@ func getClusterPreds() predicate.Funcs {
 
 	updateFunc := func(e event.UpdateEvent) bool {
 		log.Info("UpdateFunc", "managedCluster", e.ObjectNew.GetName())
-
+		if e.ObjectNew.GetName() == "local-cluster" {
+			return false
+		}
 		if e.ObjectNew.GetResourceVersion() == e.ObjectOld.GetResourceVersion() {
 			return false
 		}
@@ -66,7 +67,9 @@ func getClusterPreds() predicate.Funcs {
 
 	deleteFunc := func(e event.DeleteEvent) bool {
 		log.Info("DeleteFunc", "managedCluster", e.Object.GetName())
-
+		if e.Object.GetName() == "local-cluster" {
+			return false
+		}
 		if isAutomaticAddonInstallationDisabled(e.Object) {
 			return false
 		}
