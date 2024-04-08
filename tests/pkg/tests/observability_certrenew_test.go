daa3b3338d2cc866905c57d87a253518293fba7d
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-04-08 15:26:02 +0200

    refactor
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- tests/pkg/tests/observability_certrenew_test.go
+++ tests/pkg/tests/observability_certrenew_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Observability:", func() {
 		clusterName := utils.GetManagedClusterName(testOptions)
 		if clusterName == hubManagedClusterName {
 			namespace = hubMetricsCollectorNamespace
-			isHub = true
+			isHub = false
 		}
 	})
 
