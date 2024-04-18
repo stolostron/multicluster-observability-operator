18cbb6ed8950048b107f7fe64e345aa2e466144e
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-04-18 10:01:10 +0200

    skip test for kind
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- tests/pkg/tests/observability_endpoint_preserve_test.go
+++ tests/pkg/tests/observability_endpoint_preserve_test.go
@@ -122,6 +122,9 @@ var _ = Describe("Observability:", func() {
 	})
 
 	It("[P2][Sev2][observability][Stable] Should revert any manual changes on metrics-collector-view clusterolebinding (endpoint_preserve/g0)", func() {
+		if os.Getenv("IS_KIND_ENV") == trueStr {
+			Skip("Skip the case due to run in KinD")
+		}
 		By("Deleting metrics-collector-view clusterolebinding")
 		err, crb := utils.GetCRB(testOptions, false, "metrics-collector-view")
 		Expect(err).ToNot(HaveOccurred())
