45da5d318360c0b679e681f236a8557f419e2e34
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-04-24 15:35:33 +0200

    refactor
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- cicd-scripts/customize-mco.sh
+++ cicd-scripts/customize-mco.sh
@@ -14,7 +14,7 @@ SED_COMMAND=${SED}' -i-e -e'
 
 # Set the latest snapshot if it is not set
 source ./scripts/test-utils.sh
-LATEST_SNAPSHOT=${LATEST_SNAPSHOT:-$(get_container_image)}
+LATEST_SNAPSHOT=${LATEST_SNAPSHOT:-$(get_latest_snapshot)}
 
 # need to add this annotation due to KinD cluster resources are insufficient
 if [[ -n ${IS_KIND_ENV} ]]; then
