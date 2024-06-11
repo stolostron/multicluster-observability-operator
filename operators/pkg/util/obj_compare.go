dde0b515a24f4b266863143e53b261fc5f5781c3
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-06-11 17:34:12 +0200

    more logs
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- operators/pkg/util/obj_compare.go
+++ operators/pkg/util/obj_compare.go
@@ -71,6 +71,9 @@ func GetK8sObjWithVersion(kind, version string) runtime.Object {
 // CompareObject is used to compare two k8s objs are same or not
 func CompareObject(re1 runtime.RawExtension, re2 runtime.RawExtension) bool {
 	if re2.Object == nil {
+		log.Info("Coleen - deep equal", reflect.DeepEqual(re1.Raw, re2.Raw))
+		log.Info("Coleen - deep equal", "re1", re1.Raw)
+		log.Info("Coleen - deep equal", "re2", re2.Raw)
 		return reflect.DeepEqual(re1.Raw, re2.Raw)
 	}
 	obj1, err := GetObject(re1)
