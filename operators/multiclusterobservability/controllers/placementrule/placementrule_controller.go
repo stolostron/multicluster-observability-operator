af01a6bf3f5cd7e57c01fdfaebcf174dc95301bc
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-06-26 12:53:03 +0200

    synchronisation
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- operators/multiclusterobservability/controllers/placementrule/placementrule_controller.go
+++ operators/multiclusterobservability/controllers/placementrule/placementrule_controller.go
@@ -96,19 +96,23 @@ func (r *PlacementRuleReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	// We want to ensure that the local-cluster is always in the managedClusterList
 	// In the case when hubSelfManagement is enabled, we will delete it from the list and modify the object
 	// to cater to the use case of deploying in open-cluster-management-observability namespace
-	delete(managedClusterList, "local-cluster")
-	if _, ok := managedClusterList["local-cluster"]; !ok {
-		obj := &clusterv1.ManagedCluster{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "local-cluster",
-				Namespace: config.GetDefaultNamespace(),
-				Labels: map[string]string{
-					"openshiftVersion": "mimical",
+	if req.Name == "local-cluster" {
+		managedClusterListMutex.Lock()
+		delete(managedClusterList, "local-cluster")
+		managedClusterListMutex.Unlock()
+		if _, ok := managedClusterList["local-cluster"]; !ok {
+			obj := &clusterv1.ManagedCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "local-cluster",
+					Namespace: config.GetDefaultNamespace(),
+					Labels: map[string]string{
+						"openshiftVersion": "mimical",
+					},
 				},
-			},
+			}
+			installMetricsWithoutAddon = true
+			updateManagedClusterList(obj)
 		}
-		installMetricsWithoutAddon = true
-		updateManagedClusterList(obj)
 	}
 
 	if config.GetMonitoringCRName() == "" {
