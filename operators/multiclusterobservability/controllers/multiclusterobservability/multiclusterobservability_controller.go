f4a25ff4f4cdc0dbd4cd74a45f846397650f3d6b
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-07-22 18:31:10 +0200

    allow observability with global hub
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- operators/multiclusterobservability/controllers/multiclusterobservability/multiclusterobservability_controller.go
+++ operators/multiclusterobservability/controllers/multiclusterobservability/multiclusterobservability_controller.go
@@ -57,7 +57,6 @@ import (
 	"github.com/stolostron/multicluster-observability-operator/operators/multiclusterobservability/pkg/util"
 	"github.com/stolostron/multicluster-observability-operator/operators/pkg/deploying"
 	commonutil "github.com/stolostron/multicluster-observability-operator/operators/pkg/util"
-	operatorsutil "github.com/stolostron/multicluster-observability-operator/operators/pkg/util"
 )
 
 const (
@@ -157,19 +156,19 @@ func (r *MultiClusterObservabilityReconciler) Reconcile(ctx context.Context, req
 	StartStatusUpdate(r.Client, instance)
 
 	if _, ok := os.LookupEnv("UNIT_TEST"); !ok {
-		crdClient, err := operatorsutil.GetOrCreateCRDClient()
-		if err != nil {
-			return ctrl.Result{}, err
-		}
-		mcghCrdExists, err := operatorsutil.CheckCRDExist(crdClient, config.MCGHCrdName)
-		if err != nil {
-			return ctrl.Result{}, err
-		}
-		if mcghCrdExists {
-			// Do not start the MCO if the MCGH CRD exists
-			reqLogger.Info("MCGH CRD exists, Observability is not supported")
-			return ctrl.Result{}, nil
-		}
+		//crdClient, err := operatorsutil.GetOrCreateCRDClient()
+		//if err != nil {
+		//	return ctrl.Result{}, err
+		//}
+		//mcghCrdExists, err := operatorsutil.CheckCRDExist(crdClient, config.MCGHCrdName)
+		//if err != nil {
+		//	return ctrl.Result{}, err
+		//}
+		//if mcghCrdExists {
+		//	// Do not start the MCO if the MCGH CRD exists
+		//	reqLogger.Info("MCGH CRD exists, Observability is not supported")
+		//	return ctrl.Result{}, nil
+		//}
 	}
 
 	ingressCtlCrdExists := r.CRDMap[config.IngressControllerCRD]
