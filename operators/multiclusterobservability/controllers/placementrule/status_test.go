1c44fc9bbd92d4ebad671c318a9574ed4d21f78f
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2024-07-25 12:01:45 +0200

    update placement controller status update logic
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- operators/multiclusterobservability/controllers/placementrule/status_test.go
+++ operators/multiclusterobservability/controllers/placementrule/status_test.go
@@ -6,6 +6,7 @@ package placementrule
 
 import (
 	"context"
+	"slices"
 	"testing"
 	"time"
 
@@ -18,67 +19,178 @@ import (
 	mcov1beta1 "github.com/stolostron/multicluster-observability-operator/operators/multiclusterobservability/api/v1beta1"
 	mcov1beta2 "github.com/stolostron/multicluster-observability-operator/operators/multiclusterobservability/api/v1beta2"
 	"github.com/stolostron/multicluster-observability-operator/operators/multiclusterobservability/pkg/util"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUpdateAddonStatus(t *testing.T) {
-	maddon := &addonv1alpha1.ManagedClusterAddOn{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      util.ManagedClusterAddonName,
-			Namespace: namespace,
-		},
-		Status: addonv1alpha1.ManagedClusterAddOnStatus{},
-	}
-	scheme := runtime.NewScheme()
-	addonv1alpha1.AddToScheme(scheme)
-	mcov1beta1.AddToScheme(scheme)
-	mcov1beta2.AddToScheme(scheme)
-
-	objs := []runtime.Object{maddon}
-	c := fake.NewClientBuilder().
-		WithScheme(scheme).
-		WithRuntimeObjects(objs...).
-		WithStatusSubresource(
-			&addonv1alpha1.ManagedClusterAddOn{},
-			&mcov1beta2.MultiClusterObservability{},
-			&mcov1beta1.ObservabilityAddon{},
-		).
-		Build()
-
-	addonList := &mcov1beta1.ObservabilityAddonList{
-		Items: []mcov1beta1.ObservabilityAddon{
-			{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      obsAddonName,
-					Namespace: namespace,
+	testCases := map[string]struct {
+		currentObsAddonConditions      []mcov1beta1.StatusCondition
+		currentClusterAddonConditions  []metav1.Condition
+		expectedClusterAddonConditions []metav1.Condition
+	}{
+		"new condition should be appended": {
+			currentObsAddonConditions: []mcov1beta1.StatusCondition{
+				{
+					Type:               "Available",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
 				},
-				Status: mcov1beta1.ObservabilityAddonStatus{
-					Conditions: []mcov1beta1.StatusCondition{
-						{
-							Type:               "Available",
-							Status:             metav1.ConditionTrue,
-							LastTransitionTime: metav1.NewTime(time.Now()),
-							Reason:             "Deployed",
-							Message:            "Metrics collector deployed and functional",
-						},
-					},
+			},
+			currentClusterAddonConditions: []metav1.Condition{},
+			expectedClusterAddonConditions: []metav1.Condition{
+				{
+					Type:               "Available",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
+				},
+			},
+		},
+		"existing condition should be updated": { // updating status to false
+			currentObsAddonConditions: []mcov1beta1.StatusCondition{
+				{
+					Type:               "Available",
+					Status:             metav1.ConditionFalse,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
+				},
+			},
+			currentClusterAddonConditions: []metav1.Condition{
+				{
+					Type:               "Available",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
+				},
+			},
+			expectedClusterAddonConditions: []metav1.Condition{
+				{
+					Type:               "Available",
+					Status:             metav1.ConditionFalse,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
+				},
+			},
+		},
+		"unrelated conditions should not be updated": {
+			currentObsAddonConditions: []mcov1beta1.StatusCondition{
+				{
+					Type:               "Available",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
+				},
+			},
+			currentClusterAddonConditions: []metav1.Condition{
+				{
+					Type:               "Unrelated",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
+				},
+			},
+			expectedClusterAddonConditions: []metav1.Condition{
+				{
+					Type:               "Unrelated",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
+				},
+				{
+					Type:               "Available",
+					Status:             metav1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+					Reason:             "Deployed",
+					Message:            "It is deployed",
 				},
 			},
 		},
 	}
 
-	err := updateAddonStatus(c, *addonList)
-	if err != nil {
-		t.Fatalf("Failed to update status for managedclusteraddon: (%v)", err)
+	sortConditionsFunc := func(a, b metav1.Condition) int {
+		if a.Type < b.Type {
+			return -1
+		}
+		if a.Type > b.Type {
+			return 1
+		}
+		return 0
 	}
 
-	err = c.Get(context.TODO(), types.NamespacedName{
-		Name:      util.ManagedClusterAddonName,
-		Namespace: namespace,
-	}, maddon)
-	if err != nil {
-		t.Fatalf("Failed to get managedclusteraddon: (%v)", err)
-	}
-	if maddon.Status.Conditions == nil || len(maddon.Status.Conditions) != 1 {
-		t.Fatalf("Status not updated correctly in managedclusteraddon: (%v)", maddon)
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			scheme := runtime.NewScheme()
+			addonv1alpha1.AddToScheme(scheme)
+			mcov1beta1.AddToScheme(scheme)
+			mcov1beta2.AddToScheme(scheme)
+
+			clusterAddon := &addonv1alpha1.ManagedClusterAddOn{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      util.ManagedClusterAddonName,
+					Namespace: namespace,
+				},
+				Status: addonv1alpha1.ManagedClusterAddOnStatus{
+					Conditions: tc.currentClusterAddonConditions,
+				},
+			}
+
+			c := fake.NewClientBuilder().
+				WithScheme(scheme).
+				WithRuntimeObjects(clusterAddon).
+				WithStatusSubresource(
+					&addonv1alpha1.ManagedClusterAddOn{},
+					&mcov1beta2.MultiClusterObservability{},
+					&mcov1beta1.ObservabilityAddon{},
+				).
+				Build()
+
+			addonList := mcov1beta1.ObservabilityAddonList{
+				Items: []mcov1beta1.ObservabilityAddon{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      obsAddonName,
+							Namespace: namespace,
+						},
+						Status: mcov1beta1.ObservabilityAddonStatus{
+							Conditions: tc.currentObsAddonConditions,
+						},
+					},
+				},
+			}
+
+			err := updateAddonStatus(context.Background(), c, addonList)
+			if err != nil {
+				t.Fatalf("Failed to update status for managedclusteraddon: (%v)", err)
+			}
+
+			foundClusterAddon := &addonv1alpha1.ManagedClusterAddOn{}
+			err = c.Get(context.Background(), types.NamespacedName{
+				Name:      util.ManagedClusterAddonName,
+				Namespace: namespace,
+			}, foundClusterAddon)
+			if err != nil {
+				t.Fatalf("Failed to get managedclusteraddon: (%v)", err)
+			}
+
+			slices.SortFunc(foundClusterAddon.Status.Conditions, sortConditionsFunc)
+			slices.SortFunc(tc.expectedClusterAddonConditions, sortConditionsFunc)
+			assert.Equal(t, len(tc.expectedClusterAddonConditions), len(foundClusterAddon.Status.Conditions))
+			for i := range tc.expectedClusterAddonConditions {
+				assert.Equal(t, tc.expectedClusterAddonConditions[i].Type, foundClusterAddon.Status.Conditions[i].Type)
+				assert.Equal(t, tc.expectedClusterAddonConditions[i].Status, foundClusterAddon.Status.Conditions[i].Status)
+				assert.Equal(t, tc.expectedClusterAddonConditions[i].Reason, foundClusterAddon.Status.Conditions[i].Reason)
+				assert.Equal(t, tc.expectedClusterAddonConditions[i].Message, foundClusterAddon.Status.Conditions[i].Message)
+				assert.InEpsilon(t, tc.expectedClusterAddonConditions[i].LastTransitionTime.Unix(), foundClusterAddon.Status.Conditions[i].LastTransitionTime.Unix(), 1)
+			}
+		})
 	}
 }
