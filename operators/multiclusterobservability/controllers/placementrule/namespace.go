3c4e6516083ca02246495fc41f79cbd5de108a94
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2024-07-31 13:04:35 +0200

    use diff addon namespace for each hub
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- operators/multiclusterobservability/controllers/placementrule/namespace.go
+++ operators/multiclusterobservability/controllers/placementrule/namespace.go
@@ -5,7 +5,10 @@
 package placementrule
 
 import (
+	"context"
+	"github.com/stolostron/multicluster-observability-operator/operators/endpointmetrics/pkg/openshift"
 	"os"
+	client "sigs.k8s.io/controller-runtime/pkg/client"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -17,7 +20,14 @@ var (
 	spokeNameSpace = os.Getenv("SPOKE_NAMESPACE")
 )
 
+func getSpokeNameSpace(c client.Client) string {
+	clusterID, _ := openshift.GetClusterID(context.TODO(), c)
+	spokeNameSpace = spokeNameSpace + "-" + clusterID
+	return spokeNameSpace
+}
+
 func generateNamespace() *corev1.Namespace {
+
 	return &corev1.Namespace{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: corev1.SchemeGroupVersion.String(),
