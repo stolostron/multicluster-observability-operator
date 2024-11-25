9820434181a87807e21c51f4ab44b314b49c541b
Jacob Baungård Hansen <jacobbaungard@redhat.com>
2024-11-25 11:03:00 +0100

    Bump hypershift api version to v1beta1 (#1676)
    v1alpha1 is no longer used, so we need to update to v1beta1 to avoid the
    metrics-collector from crashing.
    
    Fixes: ACM-15628
    
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

--- operators/endpointmetrics/pkg/openshift/openshift_test.go
+++ operators/endpointmetrics/pkg/openshift/openshift_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/go-logr/logr"
 	ocinfrav1 "github.com/openshift/api/config/v1"
-	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1alpha1"
+	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
 	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/stolostron/multicluster-observability-operator/operators/endpointmetrics/pkg/openshift"
 	rbacv1 "k8s.io/api/rbac/v1"
