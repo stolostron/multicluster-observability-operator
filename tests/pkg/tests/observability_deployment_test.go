39d46436b0883a8809853c79205b57384baa2956
Jacob Baungard Hansen <jacobbaungard@redhat.com>
2024-12-19 15:59:59 +0100

    fix copyright
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

--- tests/pkg/tests/observability_deployment_test.go
+++ tests/pkg/tests/observability_deployment_test.go
@@ -1,5 +1,6 @@
-// Copyright (c) 2021 Red Hat, Inc.
+// Copyright (c) Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
+// Licensed under the Apache License 2.0
 
 package tests
 
