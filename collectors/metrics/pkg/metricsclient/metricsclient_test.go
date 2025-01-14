15933a5fac7c6c72768784cb55bd9382ca321bf6
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2025-01-14 11:25:50 +0100

    dedup __name__ label
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- collectors/metrics/pkg/metricsclient/metricsclient_test.go
+++ collectors/metrics/pkg/metricsclient/metricsclient_test.go
@@ -39,6 +39,7 @@ func Test_convertToTimeseries(t *testing.T) {
 	fooLabelName := "foo"
 	fooLabelValue1 := "bar"
 	fooLabelValue2 := "baz"
+	nameLabel := nameLabelName
 
 	emptyLabelName := ""
 
@@ -199,8 +200,12 @@ func Test_convertToTimeseries(t *testing.T) {
 					Counter:     &clientmodel.Counter{Value: &value42},
 					TimestampMs: &timestamp,
 				}, {
-					// With duplicate labels.
-					Label:       []*clientmodel.LabelPair{{Name: &fooLabelName, Value: &fooLabelValue2}, {Name: &fooLabelName, Value: &fooLabelValue2}},
+					// With duplicate labels, including the __name__ label.
+					Label: []*clientmodel.LabelPair{
+						{Name: &fooLabelName, Value: &fooLabelValue2},
+						{Name: &fooLabelName, Value: &fooLabelValue2},
+						{Name: &nameLabel, Value: &barMetricName},
+					},
 					Counter:     &clientmodel.Counter{Value: &value42},
 					TimestampMs: &timestamp,
 				}, {
