99d3e7e9f8d4d3f805b9ee010042e42c537f65ca
Jacob Baungard Hansen <jacobbaungard@redhat.com>
2025-02-10 11:28:18 +0100

    Tests: Set correct ginkgo version in e2e tests
    It would not download Ginkgo at all and the test therefore failed
    completetly to run anything.
    
    Signed-off-by: Jacob Baungard Hansen <jacobbaungard@redhat.com>

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -88,7 +88,7 @@ else
   #     rm -fr /usr/local/go
   #     tar -C /usr/local -xzf go1.21.10.linux-amd64.tar.gz
   fi
-  go install github.com/onsi/ginkgo/v2/ginkgo@2.17.1
+  go install github.com/onsi/ginkgo/v2/ginkgo@v2.17.1
   GINKGO_CMD="$(go env GOPATH)/bin/ginkgo"
 fi
 
