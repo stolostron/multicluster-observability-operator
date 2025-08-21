4003ee7f5020b152d67852b30c7f221efd51de80
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2025-08-21 16:28:40 +0200

    refactor userProject cache
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- proxy/pkg/util/user_project.go
+++ proxy/pkg/util/user_project.go
@@ -11,70 +11,95 @@ import (
 	"k8s.io/klog"
 )
 
-var userProjectInfo *UserProjectInfo
+const defaultCleanPeriod = time.Second * 60 * 5
 
 type UserProjectInfo struct {
-	sync.RWMutex
-	ProjectInfo map[string]UserProject
+	mu              sync.RWMutex
+	projectInfo     map[string]userProject
+	expiredDuration time.Duration
+	cleanPeriod     time.Duration
+	stopCh          chan struct{}
 }
 
-type UserProject struct {
+type userProject struct {
 	UserName    string
-	Timestamp   int64
-	Token       string
+	Timestamp   time.Time
 	ProjectList []string
 }
 
-func InitUserProjectInfo() {
-	if userProjectInfo == nil {
-		userProjectInfo = new(UserProjectInfo)
-		userProjectInfo.ProjectInfo = map[string]UserProject{}
+func NewUserProjectInfo(expiredDuration, cleanPeriod time.Duration) *UserProjectInfo {
+	if cleanPeriod <= 0 {
+		cleanPeriod = defaultCleanPeriod
 	}
+	upi := &UserProjectInfo{
+		projectInfo:     map[string]userProject{},
+		expiredDuration: expiredDuration,
+		cleanPeriod:     cleanPeriod,
+		stopCh:          make(chan struct{}),
+	}
+
+	go upi.autoCleanExpiredProjectInfo()
+
+	return upi
 }
 
-func NewUserProject(userName string, token string, projects []string) UserProject {
-	up := UserProject{}
-	up.UserName = userName
-	up.Timestamp = time.Now().Unix()
-	up.Token = token
-	up.ProjectList = projects
-	return up
+func (upi *UserProjectInfo) UpdateUserProject(userName string, token string, projects []string) {
+	upi.mu.Lock()
+	upi.projectInfo[token] = userProject{
+		UserName:    userName,
+		Timestamp:   time.Now(),
+		ProjectList: projects,
+	}
+	upi.mu.Unlock()
 }
 
-func UpdateUserProject(up UserProject) {
-	userProjectInfo.Lock()
-	userProjectInfo.ProjectInfo[up.Token] = up
-	userProjectInfo.Unlock()
-}
-
-func GetUserProjectList(token string) ([]string, bool) {
-	userProjectInfo.Lock()
-	defer userProjectInfo.Unlock()
-	up, ok := userProjectInfo.ProjectInfo[token]
+func (upi *UserProjectInfo) GetUserProjectList(token string) ([]string, bool) {
+	upi.mu.RLock()
+	up, ok := upi.projectInfo[token]
+	upi.mu.RUnlock()
 	if ok {
 		return up.ProjectList, true
 	}
 	return []string{}, false
 }
 
-func CleanExpiredProjectInfoJob(expiredTimeSeconds int64) {
-	InitUserProjectInfo()
-	ticker := time.NewTicker(time.Duration(time.Second * time.Duration(expiredTimeSeconds)))
+func (upi *UserProjectInfo) Stop() {
+	close(upi.stopCh)
+}
+
+func (upi *UserProjectInfo) autoCleanExpiredProjectInfo() {
+	ticker := time.NewTicker(upi.cleanPeriod)
 	defer ticker.Stop()
 
 	for {
-		<-ticker.C
-		CleanExpiredProjectInfo(expiredTimeSeconds)
+		select {
+		case <-ticker.C:
+			upi.cleanExpiredProjectInfo()
+		case <-upi.stopCh:
+			klog.Info("stopping user project info auto cleaning")
+			return
+		}
 	}
 }
 
-func CleanExpiredProjectInfo(expiredTimeSeconds int64) {
-	userProjectInfo.Lock()
-	defer userProjectInfo.Unlock()
-	for _, up := range userProjectInfo.ProjectInfo {
-		if time.Now().Unix()-up.Timestamp >= expiredTimeSeconds {
-			klog.Infof("clean %v project info", up.UserName)
-			delete(userProjectInfo.ProjectInfo, up.Token)
+func (upi *UserProjectInfo) cleanExpiredProjectInfo() {
+	var expiredTokens []string
+	upi.mu.RLock()
+	for token, up := range upi.projectInfo {
+		if time.Since(up.Timestamp) >= upi.expiredDuration {
+			expiredTokens = append(expiredTokens, token)
+		}
+	}
+	upi.mu.RUnlock()
+
+	if len(expiredTokens) > 0 {
+		upi.mu.Lock()
+		defer upi.mu.Unlock()
+		for _, token := range expiredTokens {
+			if up, ok := upi.projectInfo[token]; ok && time.Since(up.Timestamp) >= upi.expiredDuration {
+				klog.Infof("clean %v project info", up.UserName)
+				delete(upi.projectInfo, token)
+			}
 		}
 	}
 }
