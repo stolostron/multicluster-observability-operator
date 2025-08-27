8cd49f3af31da93ca0da2b9f3dd075e3075f9b31
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2025-08-27 11:17:57 +0200

    refactor of the proxy package
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- proxy/pkg/proxy/proxy.go
+++ proxy/pkg/proxy/proxy.go
@@ -5,6 +5,8 @@
 package proxy
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -25,9 +27,13 @@ import (
 )
 
 const (
-	basePath        = "/api/metrics/v1/default"
-	projectsAPIPath = "/apis/project.openshift.io/v1/projects"
-	userAPIPath     = "/apis/user.openshift.io/v1/users/~"
+	basePath               = "/api/metrics/v1/default"
+	projectsAPIPath        = "/apis/project.openshift.io/v1/projects"
+	userAPIPath            = "/apis/user.openshift.io/v1/users/~"
+	apiSeriesPath          = "/api/v1/series"
+	apiLabelNameValuesPath = "/api/v1/label/label_name/values"
+	apiQueryPath           = "/api/v1/query"
+	apiQueryRangePath      = "/api/v1/query_range"
 )
 
 // Proxy is a reverse proxy for the metrics server.
@@ -43,81 +49,113 @@ type Proxy struct {
 // NewProxy creates a new Proxy.
 func NewProxy(serverURL *url.URL, transport http.RoundTripper, apiserverHost string, upi *cache.UserProjectInfo, managedClusterInformer informer.ManagedClusterInformable, accessReviewer metricquery.AccessReviewer) (*Proxy, error) {
 	p := &Proxy{
-		metricsServerURL: serverURL,
-		proxy: &httputil.ReverseProxy{
-			Director:  proxyRequest,
-			Transport: transport,
-		},
+		metricsServerURL:       serverURL,
 		apiServerHost:          apiserverHost,
 		userProjectInfo:        upi,
 		managedClusterInformer: managedClusterInformer,
 		accessReviewer:         accessReviewer,
 	}
 
+	p.proxy = &httputil.ReverseProxy{
+		Director: func(req *http.Request) {
+			proxyRequest(req)
+			req.URL.Scheme = serverURL.Scheme
+			req.URL.Host = serverURL.Host
+			req.Host = serverURL.Host
+		},
+		Transport: transport,
+	}
+
 	return p, nil
 }
 
-func requestContainsRBACProxyLabeMetricName(req *http.Request) bool {
-	if req.Method == "POST" {
+func requestContainsRBACProxyLabelMetricName(req *http.Request) bool {
+	switch req.Method {
+	case http.MethodPost:
 		body, err := io.ReadAll(req.Body)
 		if err != nil {
 			klog.Errorf("failed to read body: %v", err)
+			req.Body = io.NopCloser(bytes.NewReader(body))
+			return false
 		}
-		req.Body = io.NopCloser(strings.NewReader(string(body)))
-		req.ContentLength = int64(len([]rune(string(body))))
+		// Replace the body so it can be read again downstream.
+		req.Body = io.NopCloser(bytes.NewReader(body))
+		req.ContentLength = int64(len(body))
 		return strings.Contains(string(body), proxyconfig.GetRBACProxyLabelMetricName())
-	} else if req.Method == "GET" {
-		queryParams := req.URL.Query()
-		return strings.Contains(queryParams.Get("match[]"), proxyconfig.GetRBACProxyLabelMetricName())
+	case http.MethodGet:
+		return strings.Contains(req.URL.Query().Get("match[]"), proxyconfig.GetRBACProxyLabelMetricName())
+	default:
+		return false
 	}
-	return false
 }
 
 func (p *Proxy) shouldModifyAPISeriesResponse(res http.ResponseWriter, req *http.Request) bool {
-	// Different Grafana versions uses different calls, we handle:
+	// Different Grafana versions use different calls, we handle:
 	// GET/POST requests for series and label_name
-	if strings.HasSuffix(req.URL.Path, "/api/v1/series") ||
-		strings.HasSuffix(req.URL.Path, "/api/v1/label/label_name/values") {
-		if requestContainsRBACProxyLabeMetricName(req) {
+	if strings.HasSuffix(req.URL.Path, apiSeriesPath) ||
+		strings.HasSuffix(req.URL.Path, apiLabelNameValuesPath) {
+		if requestContainsRBACProxyLabelMetricName(req) {
 			managedLabelList := p.managedClusterInformer.GetManagedClusterLabelList()
 
-			query := createQueryResponse(managedLabelList.RegexLabelList, proxyconfig.GetRBACProxyLabelMetricName(), req.URL.Path)
-			_, err := res.Write([]byte(query))
-			if err == nil {
-				return true
-			} else {
-				klog.Errorf("failed to write query: %v", err)
+			query, err := createQueryResponse(managedLabelList.RegexLabelList, proxyconfig.GetRBACProxyLabelMetricName(), req.URL.Path)
+			if err != nil {
+				klog.Errorf("failed to create query response: %v", err)
+				// Let the request fall through to the proxy to return a proper error.
+				return false
 			}
+
+			res.Header().Set("Content-Type", "application/json")
+			_, err = res.Write(query)
+			if err != nil {
+				klog.Errorf("failed to write query response: %v", err)
+			}
+			return true // We've handled the request.
 		}
-
 	}
-
 	return false
 }
 
-func createQueryResponse(labels []string, metricName string, urlPath string) string {
-	query := `{"status":"success","data":[`
-	for index, label := range labels {
-		if strings.HasSuffix(urlPath, "/api/v1/label/label_name/values") {
-			query += `"` + label + `"`
-		} else {
-			// series
-			query += `{"__name__":"` + metricName + `","label_name":"` + label + `"}`
+// Structs for creating a JSON response for series queries.
+type seriesData struct {
+	Name      string `json:"__name__"`
+	LabelName string `json:"label_name"`
+}
+type queryResponse struct {
+	Status string      `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
+func createQueryResponse(labels []string, metricName string, urlPath string) ([]byte, error) {
+	var data interface{}
+	if strings.HasSuffix(urlPath, apiLabelNameValuesPath) {
+		data = labels
+	} else {
+		series := make([]seriesData, len(labels))
+		for i, label := range labels {
+			series[i] = seriesData{
+				Name:      metricName,
+				LabelName: label,
+			}
 		}
-		if index != len(labels)-1 {
-			query += ","
-		}
+		data = series
 	}
-	query += `]}`
-	return query
+
+	response := queryResponse{
+		Status: "success",
+		Data:   data,
+	}
+
+	return json.Marshal(response)
 }
 
 // ServeHTTP is used to init proxy handler.
 func (p *Proxy) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	if p.preCheckRequest(req) != nil {
-		_, err := res.Write(newEmptyMatrixHTTPBody())
-		if err != nil {
-			klog.Errorf("failed to write response: %v", err)
+	if err := p.preCheckRequest(req); err != nil {
+		klog.Warningf("pre-check failed for user <%s>: %v", req.Header.Get("X-Forwarded-User"), err)
+		res.Header().Set("Content-Type", "application/json")
+		_, writeErr := res.Write(newEmptyMatrixHTTPBody())
+		if writeErr != nil {
+			klog.Errorf("failed to write response: %v", writeErr)
 		}
 		return
 	}
@@ -129,7 +167,7 @@ func (p *Proxy) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
 	req.Host = p.metricsServerURL.Host
 	req.URL.Path = path.Join(basePath, req.URL.Path)
-		(&metricquery.Modifier{
+	(&metricquery.Modifier{
 		Req:            req,
 		ReqURL:         config.GetConfigOrDie().Host + projectsAPIPath,
 		AccessReviewer: p.accessReviewer,
@@ -145,11 +183,9 @@ func (p *Proxy) preCheckRequest(req *http.Request) error {
 		token = req.Header.Get("Authorization")
 		if token == "" {
 			return errors.New("found unauthorized user")
-		} else {
-			// Remove Bearer from token if present
-			token = strings.TrimPrefix(token, "Bearer ")
-			req.Header.Set("X-Forwarded-Access-Token", token)
 		}
+		token = strings.TrimPrefix(token, "Bearer ")
+		req.Header.Set("X-Forwarded-Access-Token", token)
 	}
 
 	userName := req.Header.Get("X-Forwarded-User")
@@ -161,17 +197,14 @@ func (p *Proxy) preCheckRequest(req *http.Request) error {
 		userName = util.GetUserName(token, userAPIURL)
 		if userName == "" {
 			return errors.New("failed to find user name")
-		} else {
-			req.Header.Set("X-Forwarded-User", userName)
 		}
+		req.Header.Set("X-Forwarded-User", userName)
 	}
 
-	_, ok := p.userProjectInfo.GetUserProjectList(token)
-	if !ok {
+	if _, ok := p.userProjectInfo.GetUserProjectList(token); !ok {
 		userProjectsURL, err := url.JoinPath(p.apiServerHost, projectsAPIPath)
 		if err != nil {
 			return fmt.Errorf("failed to join the user projects api path with the apiserver host: %w", err)
-
 		}
 		projectList := util.FetchUserProjectList(token, userProjectsURL)
 		p.userProjectInfo.UpdateUserProject(userName, token, projectList)
@@ -190,9 +223,9 @@ func newEmptyMatrixHTTPBody() []byte {
 
 func proxyRequest(r *http.Request) {
 	if r.Method == http.MethodGet {
-		if strings.HasSuffix(r.URL.Path, "/api/v1/query") ||
-			strings.HasSuffix(r.URL.Path, "/api/v1/query_range") ||
-			strings.HasSuffix(r.URL.Path, "/api/v1/series") {
+		if strings.HasSuffix(r.URL.Path, apiQueryPath) ||
+			strings.HasSuffix(r.URL.Path, apiQueryRangePath) ||
+			strings.HasSuffix(r.URL.Path, apiSeriesPath) {
 			r.Method = http.MethodPost
 			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 			r.Body = io.NopCloser(strings.NewReader(r.URL.RawQuery))
