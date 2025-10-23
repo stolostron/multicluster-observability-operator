3f0539db2674bdef6b7f9ab23bad4a0697ad2b76
Daniel Buchanan <dbuchana@redhat.com>
2025-10-23 14:05:12 -0400

    Manage different cases for options yaml
    Signed-off-by: Daniel Buchanan <dbuchana@redhat.com>

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -69,9 +69,11 @@ if [[ -n ${IS_KIND_ENV} ]]; then
 fi
 printf "\n  clusters:" >>${OPTIONSFILE}
 
-if [[ -n ${IS_KIND_ENV} ]]; then
+if [[ ${CLUSTERPOOL_MANAGED_COUNT} -eq 0 ]]; then
   printf "\n    - name: ${cluster_name}" >>${OPTIONSFILE}
-  printf "\n      clusterServerURL: ${clusterServerURL}" >>${OPTIONSFILE}
+  if [[ -n ${IS_KIND_ENV} ]]; then
+    printf "\n      clusterServerURL: ${clusterServerURL}" >>${OPTIONSFILE}
+  fi
   printf "\n      baseDomain: ${base_domain}" >>${OPTIONSFILE}
   printf "\n      kubeconfig: ${kubeconfig_hub_path}" >>${OPTIONSFILE}
   printf "\n      kubecontext: ${kubecontext}" >>${OPTIONSFILE}
@@ -80,9 +82,8 @@ fi
 if [[ ! -z "${SHARED_DIR}" ]]; then
   for ((i=1 ; i <= CLUSTERPOOL_MANAGED_COUNT ; i++)); do
     if [[ -f "${SHARED_DIR}/managed-${i}.kc" ]]; then
-      # Managed cluster kubeconfig exists in CI environment
       kubeconfig_managed_path="${SHARED_DIR}/managed-${i}.kc"
-      # Get managed cluster context and server URL
+      # Get managed cluster context, server URL, and base domain
       managed_kubecontext=$(KUBECONFIG="${kubeconfig_managed_path}" kubectl config current-context)
       managed_clusterServerURL=$(KUBECONFIG="${kubeconfig_managed_path}" kubectl config view -o jsonpath="{.clusters[0].cluster.server}")
       managed_app_domain=$(KUBECONFIG="${kubeconfig_managed_path}" kubectl -n openshift-ingress-operator get ingresscontrollers default -ojsonpath='{.status.domain}')
