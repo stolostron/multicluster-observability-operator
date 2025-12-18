6854547f6bd326731a757888f197ade45eee8dbc
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2025-12-18 21:39:42 +0100

    fix lint
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- operators/multiclusterobservability/pkg/certificates/signer_test.go
+++ operators/multiclusterobservability/pkg/certificates/signer_test.go
@@ -10,10 +10,15 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
+	"errors"
 	"os"
 	"testing"
 
 	certificatesv1 "k8s.io/api/certificates/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func init() {
@@ -56,4 +61,24 @@ func TestSign(t *testing.T) {
 	if res == nil {
 		t.Fatal("Failed to sign CSR, nil result")
 	}
+}
+
+func getClient(s *runtime.Scheme) (client.Client, error) {
+	if os.Getenv("TEST") != "" {
+		c := fake.NewClientBuilder().Build()
+		return c, nil
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		return nil, errors.New("failed to create the kube config")
+	}
+	options := client.Options{}
+	if s != nil {
+		options = client.Options{Scheme: s}
+	}
+	c, err := client.New(config, options)
+	if err != nil {
+		return nil, errors.New("failed to create the kube client")
+	}
+	return c, nil
 }
