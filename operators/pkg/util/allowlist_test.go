66f4296bae9415882736e3efa5287d133cfa1f4c
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2026-01-21 12:35:07 +0100

    fix format
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- operators/pkg/util/allowlist_test.go
+++ operators/pkg/util/allowlist_test.go
@@ -6,11 +6,11 @@ package util
 
 import (
 	"reflect"
+	"slices"
 	"testing"
 
 	"github.com/stolostron/multicluster-observability-operator/operators/multiclusterobservability/pkg/config"
 	operatorconfig "github.com/stolostron/multicluster-observability-operator/operators/pkg/config"
-	"slices"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
