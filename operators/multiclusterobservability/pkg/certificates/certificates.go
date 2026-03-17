3328e29e53e5fdaad672b3c6217583786e435f76
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2026-03-17 09:57:22 +0100

    fix unit tests
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- operators/multiclusterobservability/pkg/certificates/certificates.go
+++ operators/multiclusterobservability/pkg/certificates/certificates.go
@@ -266,12 +266,13 @@ func createCertSecret(c client.Client,
 				if err != nil {
 					log.Error(err, "Failed to parse the server certificate, renew it")
 					isRenew = true
-				}
-				// to handle upgrade scenario in which hosts maybe update
-				for _, dnsString := range dns {
-					if !slices.Contains(serverCrt.DNSNames, dnsString) {
-						isRenew = true
-						break
+				} else {
+					// to handle upgrade scenario in which hosts maybe update
+					for _, dnsString := range dns {
+						if !slices.Contains(serverCrt.DNSNames, dnsString) {
+							isRenew = true
+							break
+						}
 					}
 				}
 			}
