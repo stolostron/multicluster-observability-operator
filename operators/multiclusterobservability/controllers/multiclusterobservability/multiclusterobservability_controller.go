20c0c2ddcbb3812166c13df6b058bb6cf88cb80d
George Aeillo <gaeillo@redhat.com>
2026-05-28 14:32:16 -0400

    Fix indent error
    Signed-off-by: George Aeillo <gaeillo@redhat.com>

--- operators/multiclusterobservability/controllers/multiclusterobservability/multiclusterobservability_controller.go
+++ operators/multiclusterobservability/controllers/multiclusterobservability/multiclusterobservability_controller.go
@@ -390,6 +390,7 @@ func (r *MultiClusterObservabilityReconciler) Reconcile(ctx context.Context, req
 				return ctrl.Result{}, fmt.Errorf("failed to delete ClusterManagementAddOn %s: %w", config.MultiClusterObservabilityAddon, err)
 			}
 		}
+	}
 
 	_, err = r.ensureOpenShiftNamespaceLabel(ctx, instance)
 	if err != nil {
