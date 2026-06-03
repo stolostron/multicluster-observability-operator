d0ec366959d7084b3ecc6026f2cbb15c0754500b
Daniel Buchanan <dbuchana@redhat.com>
2026-06-03 14:05:47 -0400

    fixing lint and kind cert error
    Signed-off-by: Daniel Buchanan <dbuchana@redhat.com>

--- operators/endpointmetrics/pkg/rendering/renderer_test.go
+++ operators/endpointmetrics/pkg/rendering/renderer_test.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"os"
 	"path"
+	"strings"
 	"testing"
 
 	operatorconfig "github.com/stolostron/multicluster-observability-operator/operators/pkg/config"
@@ -59,4 +60,52 @@ func TestRender(t *testing.T) {
 	}
 
 	assert.Greater(t, len(objs), 2)
+}
+
+func TestRenderAlertmanagerConfig(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("failed to get working dir %v", err)
+	}
+	templatesPath := path.Join(path.Dir(path.Dir(wd)), "manifests")
+	os.Setenv(templatesutil.TemplatesPathEnvVar, templatesPath)
+	defer os.Unsetenv(templatesutil.TemplatesPathEnvVar)
+
+	const hubClusterID = "test"
+	hubInfo := &operatorconfig.HubInfo{
+		ClusterName:              "foo",
+		ObservatoriumAPIEndpoint: "testing.com",
+		AlertmanagerEndpoint:     "testing.com",
+		HubClusterID:             hubClusterID,
+	}
+	c := fake.NewClientBuilder().WithRuntimeObjects([]runtime.Object{getAllowlistCM("test-ns")}...).Build()
+
+	objs, err := Render(context.Background(), rendererutil.NewRenderer(), c, hubInfo, "test-ns")
+	if err != nil {
+		t.Fatalf("failed to render endpoint templates: %v", err)
+	}
+
+	var amConfig string
+	for _, obj := range objs {
+		if obj.GetKind() == "Secret" && obj.GetName() == "prometheus-alertmanager" {
+			amConfig = obj.Object["stringData"].(map[string]any)["alertmanager.yaml"].(string)
+			break
+		}
+	}
+	if amConfig == "" {
+		t.Fatal("prometheus-alertmanager secret not found in rendered objects")
+	}
+
+	// check for mtls and hub id
+	want := []string{
+		"/etc/prometheus/secrets/observability-alertmanager-accessor-" + hubClusterID + "/token",
+		"/etc/prometheus/secrets/obs-alertmanager-mtls-ca-" + hubClusterID + "/ca.crt",
+		"/etc/prometheus/secrets/obs-alertmanager-mtls-cert-" + hubClusterID + "/tls.crt",
+		"/etc/prometheus/secrets/obs-alertmanager-mtls-cert-" + hubClusterID + "/tls.key",
+	}
+	for _, path := range want {
+		if !strings.Contains(amConfig, path) {
+			t.Fatalf("alertmanager.yaml missing path %q:\n%s", path, amConfig)
+		}
+	}
 }
